75f1fc2f5f3ba305a487857d163d73043b09fdca
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-04 12:57:11 +0530

    Store previous seen index as well

--- Jan_26/4_3_Longest_Substring_wo_repeat_chars.py
+++ Jan_26/4_3_Longest_Substring_wo_repeat_chars.py
@@ -2,15 +2,14 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         freq, ans, i, j = {}, 0, 0, 0
         while j < len(s):
-            if s[j] not in freq or freq[s[j]] == 0:
-                freq[s[j]] = 1
+            if s[j] not in freq or freq[s[j]][0] == 0:
+                freq[s[j]] = [1, j]
                 ans = max(ans, j - i + 1)
                 j += 1
             else:
-                while freq[s[j]] == 1:
-                    freq[s[i]] -= 1
-                    i += 1
+                i = max(i, freq[s[j]][1] + 1)
+                freq[s[j]][0] = 0
         return ans
 
 
-print(Solution().lengthOfLongestSubstring(s = "pwwkew"))
+print(Solution().lengthOfLongestSubstring(s = "abba"))
