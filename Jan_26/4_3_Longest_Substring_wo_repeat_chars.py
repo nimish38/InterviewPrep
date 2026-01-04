54e23d0766dcc06b4367d35d7fc86e5355aabb41
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-04 13:12:54 +0530

    deletion first little optimal

--- Jan_26/4_3_Longest_Substring_wo_repeat_chars.py
+++ Jan_26/4_3_Longest_Substring_wo_repeat_chars.py
@@ -2,14 +2,13 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         freq, ans, i, j = {}, 0, 0, 0
         while j < len(s):
-            if s[j] not in freq or freq[s[j]][0] == 0:
-                freq[s[j]] = [1, j]
-                ans = max(ans, j - i + 1)
-                j += 1
-            else:
-                i = max(i, freq[s[j]][1] + 1)
-                freq[s[j]][0] = 0
+            while s[j] in freq:
+                del freq[s[i]]
+                i += 1
+            freq[s[j]] = 1
+            ans = max(ans, j - i + 1)
+            j += 1
         return ans
 
 
-print(Solution().lengthOfLongestSubstring(s = "abba"))
+print(Solution().lengthOfLongestSubstring(s = "pwwkew"))
