3dadba8f1f4d4c538e23245ace72265dc776d939
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-09 13:54:24 +0530

    use startswith to get prefix

--- Jan_26/9_14_Longest_common_prefix.py
+++ Jan_26/9_14_Longest_common_prefix.py
@@ -1,19 +1,12 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        strs.sort(key=lambda x: len(x))
-        ref, best = strs[0], len(strs[0])
+        ref = strs[0]
         for i in range(1, len(strs)):
-            common = 0
-            for j in range(min(best, len(strs[i]))):
-                if ref[j] != strs[i][j]:
-                    if j == 0:
-                        return ""
-                    else:
-                        break
-                else:
-                    common += 1
-            best = min(best, common)
-        return ref[:best]
+            while not strs[i].startswith(ref):
+                ref = ref[:-1]
+            if ref == "":
+                return ref
+        return ref
 
 
 print(Solution().longestCommonPrefix(strs =["abc", "ab"]))
