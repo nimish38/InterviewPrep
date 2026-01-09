f09d9a6fbc9b9c80e64f0dd1c280d870b5721865
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-09 13:47:25 +0530

    use common to keep count of common chars

--- Jan_26/9_14_Longest_common_prefix.py
+++ Jan_26/9_14_Longest_common_prefix.py
@@ -2,12 +2,17 @@ class Solution(object):
     def longestCommonPrefix(self, strs):
         ref, best = strs[0], len(strs[0])
         for i in range(1, len(strs)):
+            common = 0
             for j in range(min(best, len(strs[i]))):
                 if ref[j] != strs[i][j]:
                     if j == 0:
                         return ""
                     else:
-                        best = min(best, j)
-        return best
+                        break
+                else:
+                    common += 1
+            best = min(best, common)
+        return ref[:best]
 
 
+print(Solution().longestCommonPrefix(strs =["abc", "ab"]))
