0fe90fb76a496706a0a83cd8d9f258275a4aaa77
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-10 13:31:37 +0530

    use 2 pointer solution like binary search and use pruning

--- Jan_26/10_15_3Sum.py
+++ Jan_26/10_15_3Sum.py
@@ -5,16 +5,21 @@ class Solution(object):
         for i in range(n - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            seen, target = {}, -nums[i]
-            for j in range(i + 1, n):
-                if target - nums[j] in seen:
-                    triplet = sorted([nums[i], nums[seen[target - nums[j]]], nums[j]])
-                    if triplet not in res:
-                        res.append(triplet)
-                    del seen[target - nums[j]]
-                    continue
+            target, j, k = -nums[i], i + 1, n -1
+            while j < k:
+                value = nums[j] + nums[k]
+                if value == target:
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                elif value > target:
+                    k -= 1
                 else:
-                    seen[nums[j]] = j
+                    j += 1
         return res
 
-print(Solution().threeSum(nums = [2,-4,1,2,-2,2,0,2]))
+print(Solution().threeSum(nums = [-4,2,2,1,3]))
