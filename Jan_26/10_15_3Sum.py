fbc368d231cb68d50624eb84a76bfdc999c36105
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-10 13:00:42 +0530

    sort to avoid duplicates

--- Jan_26/10_15_3Sum.py
+++ Jan_26/10_15_3Sum.py
@@ -1,7 +1,10 @@
 class Solution(object):
     def threeSum(self, nums):
         res, n = [], len(nums)
+        nums.sort()
         for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             seen, target = {}, -nums[i]
             for j in range(i + 1, n):
                 if target - nums[j] in seen:
@@ -10,3 +13,6 @@ class Solution(object):
                 else:
                     seen[nums[j]] = j
         return res
+
+
+print(Solution().threeSum(nums = [-1,0,1,2,-1,-4]))
