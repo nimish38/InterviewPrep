2d2c119b6c02023ad25b65bda62370ac48105676
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-10 13:09:18 +0530

    sort the solution to check if it exists

--- Jan_26/10_15_3Sum.py
+++ Jan_26/10_15_3Sum.py
@@ -1,18 +1,17 @@
 class Solution(object):
     def threeSum(self, nums):
         res, n = [], len(nums)
-        nums.sort()
         for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
             seen, target = {}, -nums[i]
             for j in range(i + 1, n):
                 if target - nums[j] in seen:
-                    res.append([nums[i], nums[seen[target - nums[j]]], nums[j]])
+                    triplet = sorted([nums[i], nums[seen[target - nums[j]]], nums[j]])
+                    if triplet not in res:
+                        res.append(triplet)
                     continue
                 else:
                     seen[nums[j]] = j
         return res
 
 
-print(Solution().threeSum(nums = [-1,0,1,2,-1,-4]))
+print(Solution().threeSum(nums = [2,-4,1,2,-2,2,0,2]))
