e934e492510e77284feb7ee1918cd7fb77e95dda
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-11 12:25:45 +0530

    same as 3 pointer with additional for loop

---
+++ Jan_26/11_18_4Sum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        n, res, j = len(nums), [], 1
+        nums.sort()
+        for i in range(n - 3):
+            while j < n - 2:
+                k, l = j + 1, n - 1
+                while k < l:
+                    value = nums[i] + nums[j] + nums[k] + nums[l]
+                    if value == target:
+                        while k < l and nums[k] == nums[k + 1]:
+                            k += 1
+                        while k < l and nums[l] == nums[l - 1]:
+                            l -= 1
+                        res.append([nums[i], nums[j], nums[k], nums[l]])
+                        k += 1
+                        l -= 1
+                    elif value < target:
+                        k += 1
+                    else:
+                        l -= 1
+                while j < n - 2 and nums[j] == nums[j + 1]:
+                    j += 1
+                j += 1
+        return res
+
+
+print(Solution().fourSum(nums = [1,0,-1,0,-2,2], target = 0))
