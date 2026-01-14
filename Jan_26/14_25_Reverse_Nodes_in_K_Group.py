09f24fac4995abf7e9c919c5bfda04c8133be94f
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-14 12:40:30 +0530

    keep adjusting heads and tails

--- Jan_26/14_25_Reverse_Nodes_in_K_Group.py
+++ Jan_26/14_25_Reverse_Nodes_in_K_Group.py
@@ -7,6 +7,18 @@ class Solution(object):
     def reverseKGroup(self, head, k):
         if k == 1:
             return head
+        dummy = prev_tail = ListNode(-1)
+        curr = head
+        while True:
+            for _ in range(k):
+                if not curr:
+                    return dummy.next
+                curr = curr.next
+            new_head, new_tail = self.reverseLL(prev_tail, k)
+            prev_tail.next = new_head
+            new_tail.next = curr
+            prev_tail = new_tail
+
 
     def reverseLL(self, node, k):
         first, second = node, node.next
