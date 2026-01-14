1e0955e4d550e099bfe6d6556923543ba95e6989
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-14 13:12:37 +0530

    debug and fix

--- Jan_26/14_25_Reverse_Nodes_in_K_Group.py
+++ Jan_26/14_25_Reverse_Nodes_in_K_Group.py
@@ -8,17 +8,16 @@ class Solution(object):
         if k == 1:
             return head
         dummy = prev_tail = ListNode(-1)
-        curr = head
+        curr, prev_tail.next = head, head
         while True:
             for _ in range(k):
                 if not curr:
                     return dummy.next
                 curr = curr.next
-            new_head, new_tail = self.reverseLL(prev_tail, k)
+            new_head, new_tail = self.reverseLL(prev_tail.next, k)
             prev_tail.next = new_head
             new_tail.next = curr
             prev_tail = new_tail
-
 
     def reverseLL(self, node, k):
         first, second = node, node.next
@@ -28,3 +27,17 @@ class Solution(object):
             second.next = first
             first, second = second, temp
         return first, tail
+
+    def getLinkedList(self, arr):
+        head = ListNode(-1)
+        curr = head
+        for elem in arr:
+            curr.next = ListNode(elem)
+            curr = curr.next
+        return head.next
+
+
+s = Solution()
+a = s.getLinkedList([1,2,3,4,5])
+x = s.reverseKGroup(a, 3)
+print(x)
