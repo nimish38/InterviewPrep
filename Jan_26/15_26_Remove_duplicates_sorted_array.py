421a5bb58d5de1485ba7f6543dc1c1fc7e21b5ae
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-15 20:38:45 +0530

    set solution

--- Jan_26/15_26_Remove_duplicates_sorted_array.py
+++ Jan_26/15_26_Remove_duplicates_sorted_array.py
@@ -1,11 +1,6 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        k = 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                nums[k] = nums[i]
-                k += 1
-        return k, nums
-
+        nums = list(set(nums))
+        return len(nums), nums
 
 print(Solution().removeDuplicates(nums = [0,0,1,1,1,2,2,3,3,4]))
