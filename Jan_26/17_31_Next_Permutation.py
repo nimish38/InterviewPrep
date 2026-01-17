7564c2cbaf92d890c5a171206966117233de9bae
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-17 21:20:23 +0530

    2 pointer attempt with reversing once msb swap

--- Jan_26/17_31_Next_Permutation.py
+++ Jan_26/17_31_Next_Permutation.py
@@ -1,14 +1,30 @@
 class Solution(object):
     def nextPermutation(self, nums):
+        def reverse(ind):
+            i, j = ind + 1, len(nums) - 1
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
         i, j = len(nums) - 2, len(nums) - 1
-        while i >= 0 and nums[i] > nums[j]:
-            i -= 1
-        if i < 0:
-            return nums[::-1]
-        left = nums[j]
-        while i < len(nums):
-            temp = nums[i]
-            nums[i], left = left, temp
-            i += 1
+        while j >= 0:
+            while i >= 0 and nums[i] > nums[j]:
+                i -= 1
+            if i >= 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                reverse(i)
+                return nums
+            else:
+                j = j - 1
+                i = j - 1
+
+        reverse(-1)
         return nums
 
+
+x = [3,2,1]
+# for _ in range(12):
+#     x = Solution().nextPermutation(x)
+#     print(x)
+print(Solution().nextPermutation(x))
