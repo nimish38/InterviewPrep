4954152de66f9bce4602ec74afce244fb90f4eac
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-18 14:13:10 +0530

    debug and fix row - col conflits, sub box conflicts yet to fix

--- Jan_26/18_37_Sudoku_Solver.py
+++ Jan_26/18_37_Sudoku_Solver.py
@@ -8,24 +8,32 @@ class Solution(object):
                     filled += 1
                     col[j].add(board[i][j])
                     row[i].add(board[i][j])
-        empty = 81 - filled
+        self.empty = 81 - filled
 
         def solve():
-            if empty == 0:
+            if self.empty == 0:
                 return True
             for i in range(9):
                 for j in range(9):
                     if board[i][j] == '.':
                         for k in range(1, 10):
-                            if k not in row[i] and k not in col[j]:
-                                board[i][j] = k
+                            if chr(k) not in row[i] and chr(k) not in col[j]:
+                                board[i][j] = chr(k)
                                 col[j].add(board[i][j])
                                 row[i].add(board[i][j])
+                                self.empty -= 1
                                 if solve():
-                                    return board
+                                    return True
                                 col[j].remove(board[i][j])
                                 row[i].remove(board[i][j])
+                                self.empty += 1
                         return False
             return False
             
-        return solve()
+        solve()
+        return board
+    
+
+print(Solution().solveSudoku(board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],
+                                      ["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],
+                                      [".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]))
