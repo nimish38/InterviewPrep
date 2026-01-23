dd063145d86c023c7b4187bdb85a72b4ebd41068
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-24 01:25:35 +0530

    using sorting after append

--- Jan_26/20_88_Merge_Sorted_Lists.py
+++ Jan_26/20_88_Merge_Sorted_Lists.py
@@ -1,18 +1,7 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        m, n, j = m - 1, n - 1, m + n - 1
-        while m >= 0 and n >= 0:
-            if nums1[m] < nums2[n]:
-                nums1[j] = nums2[n]
-                n -= 1
-            else:
-                nums1[j] = nums1[m]
-                m -= 1
-            j -= 1
-        while n >= 0:
-            nums1[j] = nums2[n]
-            j -= 1
-            n -= 1
-        return nums1
+        nums1 = nums1[:m]
+        nums1 += nums2
+        nums1.sort()
 
 print(Solution().merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3))
