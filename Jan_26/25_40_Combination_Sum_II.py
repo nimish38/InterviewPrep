46fe355528450b9255ad1b866dbbd2df111582db
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-25 16:00:27 +0530

    pruning to optimise

--- Jan_26/25_40_Combination_Sum_II.py
+++ Jan_26/25_40_Combination_Sum_II.py
@@ -3,18 +3,19 @@ class Solution(object):
         res = []
         candidates.sort()
         def solve(ind, curr, combo):
-            if curr == target:
+            if curr == 0:
                 res.append(list(combo))
                 return
             for i in range(ind, len(candidates)):
                 if i > ind and candidates[i] == candidates[i - 1]:
                     continue
-                if curr + candidates[i] <= target:
-                    combo.append(candidates[i])
-                    solve(i + 1, curr + candidates[i], combo)
-                    combo.pop()
+                if candidates[i] > curr:
+                    break
+                combo.append(candidates[i])
+                solve(i + 1, curr - candidates[i], combo)
+                combo.pop()
 
-        solve(0, 0, [])
+        solve(0, target, [])
         return res
 
 
