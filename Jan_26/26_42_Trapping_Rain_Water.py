e619cfde832ff7dc25d0e4dc2992bb15f2325c0d
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-26 13:56:26 +0530

    2 pointer optimal solution

--- Jan_26/26_42_Trapping_Rain_Water.py
+++ Jan_26/26_42_Trapping_Rain_Water.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def trap(self, height):
         n, water = len(height), 0
-        left_max, right_max = [0] * n, [0] * n
-        for i in range(1, n):
-            left_max[i] = max(left_max[i - 1], height[i - 1])
-            right_max[n - i - 1] = max(right_max[n - i], height[n - i])
-        for i in range(1, n - 1):
-            val = min(left_max[i], right_max[i])
-            water += max(0, val - height[i])
+        left, right, left_max, right_max = 0, n - 1, height[0], height[-1]
+        while left < right:
+            if left_max <= right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+
         return water
 
 print(Solution().trap(height = [4,2,0,3,2,5]))
