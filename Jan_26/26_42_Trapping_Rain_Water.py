87bdbda9a68710dc7048a057e69208f4e9842611
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-26 13:17:15 +0530

    test and run

--- Jan_26/26_42_Trapping_Rain_Water.py
+++ Jan_26/26_42_Trapping_Rain_Water.py
@@ -5,7 +5,9 @@ class Solution(object):
         for i in range(1, n):
             left_max[i] = max(left_max[i - 1], height[i - 1])
             right_max[n - i - 1] = max(right_max[n - i], height[n - i])
-        for i in range(n):
+        for i in range(1, n - 1):
             val = min(left_max[i], right_max[i])
             water += max(0, val - height[i])
         return water
+
+print(Solution().trap(height = [4,2,0,3,2,5]))
