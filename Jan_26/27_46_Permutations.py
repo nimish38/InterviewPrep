4602c5dd85f15c127f37c4f4b152339d1772a3a4
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-27 22:49:59 +0530

    using dictionary to track all elements

--- Jan_26/27_46_Permutations.py
+++ Jan_26/27_46_Permutations.py
@@ -4,13 +4,21 @@ class Solution(object):
         def solve(ind):
             if ind >= n:
                 return
-            for i in range(ind, n - 1):
-                for j in range(i + 1, n):
+            for i in range(ind, n):
+                for j in range(i + 1 , n):
                     nums[i], nums[j] = nums[j], nums[i]
                     res.append(nums.copy())
                     solve(ind + 1)
                     nums[i], nums[j] = nums[j], nums[i]
-        solve(0)
+        def permute(curr):
+            if len(curr) == n:
+                res.append(list(curr.keys()))
+            for i in nums:
+                if i not in curr:
+                    curr[i] = i
+                    permute(curr)
+                    del curr[i]
+        permute({})
         return res
 
 print(Solution().permute(nums = [1,2,3]))
