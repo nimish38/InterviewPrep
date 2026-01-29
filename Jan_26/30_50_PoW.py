e0d85b190126fb2ce737d816a8abbd8c91187440
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-29 20:08:35 +0530

    recursion with memoization

--- Jan_26/30_50_PoW.py
+++ Jan_26/30_50_PoW.py
@@ -5,12 +5,16 @@ class Solution(object):
         if n < 0:
             x = 1 / x
             n = -n
+        memo = {}
         def solve(i):
             if i == 1:
                 return x
-            if i % 2:
-                return x * solve(i // 2) * solve( i // 2)
-            return solve(i // 2) * solve( i // 2)
+            if i not in memo:
+                if i % 2:
+                    memo[i] = x * solve(i // 2) * solve( i // 2)
+                else:
+                    memo[i] =  solve(i // 2) * solve( i // 2)
+            return memo[i]
         return solve(n)
 
 print(Solution().myPow(x = 2.10000, n = 3))
