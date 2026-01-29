54396f4bf57c044ba821bdb674b350c19fe833de
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-29 20:06:58 +0530

    using recursion to break powers

--- Jan_26/30_50_PoW.py
+++ Jan_26/30_50_PoW.py
@@ -1,5 +1,16 @@
 class Solution(object):
     def myPow(self, x, n):
-        return x ** n
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+        def solve(i):
+            if i == 1:
+                return x
+            if i % 2:
+                return x * solve(i // 2) * solve( i // 2)
+            return solve(i // 2) * solve( i // 2)
+        return solve(n)
 
 print(Solution().myPow(x = 2.10000, n = 3))
