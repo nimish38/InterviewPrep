33ac2d5d96abe0c20b93cf85a02a51ab25890417
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-30 22:37:51 +0530

    make formatting on the fly

--- Jan_26/30_51_N_Queens.py
+++ Jan_26/30_51_N_Queens.py
@@ -1,36 +1,34 @@
 class Solution(object):
     def solveNQueens(self, n):
-        board, ans, col, diag, anti = [[0]*n for _ in range(n)], [], set(), set(), set()
-        def format_board(chess):
-            res = []
-            for _ in range(n):
-                val = ''
-                for j in range(n):
-                    if chess[_][j]:
-                        val += 'Q'
-                    else:
-                        val += '.'
-                res.append(val)
-            ans.append(res)
+        res = []
+        board = [["."] * n for _ in range(n)]
 
-        def solve(r):
-            if r >= n:
-                format_board(board)
+        cols = set()
+        diag1 = set()  # r - c
+        diag2 = set()  # r + c
+
+        def backtrack(r):
+            if r == n:
+                res.append(["".join(row) for row in board])
                 return
-            for c in range(n):
-                di, an = r + c, r - c
-                if c not in col and di not in diag and an not in anti:
-                    col.add(c)
-                    diag.add(di)
-                    anti.add(an)
-                    board[r][c] = 1
-                    solve(r + 1)
-                    col.remove(c)
-                    diag.remove(di)
-                    anti.remove(an)
-                    board[r][c] = 0
 
-        solve(0)
-        return ans
+            for c in range(n):
+                if c in cols or (r - c) in diag1 or (r + c) in diag2:
+                    continue
+
+                board[r][c] = "Q"
+                cols.add(c)
+                diag1.add(r - c)
+                diag2.add(r + c)
+
+                backtrack(r + 1)
+
+                board[r][c] = "."
+                cols.remove(c)
+                diag1.remove(r - c)
+                diag2.remove(r + c)
+
+        backtrack(0)
+        return res
 
 print(Solution().solveNQueens(n = 4))
