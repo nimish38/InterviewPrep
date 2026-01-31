1877dc5a62913d49f69c4505b5490504ebda9106
nimish38 <nimish.v.shinde60@gmail.com>
2026-01-31 12:25:53 +0530

    for all negative case

--- Jan_26/31_56_Maximum_Subarray.py
+++ Jan_26/31_56_Maximum_Subarray.py
@@ -1,12 +1,16 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        n, curr, best = len(nums), 0, 0
+        n, curr, best, flag, biggest = len(nums), 0, 0, False, float('-inf')
         for i in range(n):
             if nums[i] + curr < 0:
                 curr = 0
             else:
+                flag = True
                 curr += nums[i]
             best = max(best, curr)
+            biggest = max(biggest, nums[i])
+        if not flag:
+            return biggest
         return max(curr, best)
 
 print(Solution().maxSubArray(nums = [-2,1,-3,4,-1,2,1,-5,4]))
