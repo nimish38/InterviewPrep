9e516a510984e12ee4242135cd6e8e84025dffc2
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-03 00:12:28 +0530

    Use mathematics to find kth permutation

--- Feb_26/2_60_Permutation_Sequence.py
+++ Feb_26/2_60_Permutation_Sequence.py
@@ -1,34 +1,18 @@
-import math
 class Solution(object):
     def getPermutation(self, n, k):
-        arr = []
-        for i in range(1, n + 1):
+        arr, fact, res = [], 1, ""
+        for i in range(1, n):
             arr.append(i)
-
-        def rev(x, y):
-            while x < y:
-                arr[x], arr[y] = arr[y], arr[x]
-                x += 1
-                y -= 1
-
-        def permute():
-            j = n - 1
-            while j > 0 and arr[j] < arr[j - 1]:
-                j -= 1
-            rev(j, n - 1)
-            x = j
-            for x in range(j, n):
-                if arr[x] > arr[j - 1]:
-                    break
-            arr[j - 1], arr[x] = arr[x], arr[j - 1]
-
-
-        if k == math.factorial(n):
-            rev(0, n - 1)
-            return "".join(map(str, arr))
-        for i in range(k - 1):
-            permute()
-        return "".join(map(str, arr))
-
+            fact *= i
+        arr.append(n)
+        k -= 1
+        while True:
+            num = arr[k // fact]
+            arr.remove(num)
+            res += str(num)
+            if len(arr) == 0:
+                return res
+            k %= fact
+            fact //= len(arr)
 
 print(Solution().getPermutation(n = 4, k = 9))
