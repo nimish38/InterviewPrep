3a35c59ef36c33b7537d44b19a75683a7254355d
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-05 18:55:06 +0530

    Top down approach

--- Feb_26/5_64_Minimum_Path_Sum.py
+++ Feb_26/5_64_Minimum_Path_Sum.py
@@ -2,21 +2,18 @@ class Solution(object):
     def minPathSum(self, grid):
         m, n = len(grid), len(grid[0])
         memo = [[-1]* n for _ in range(m)]
-        memo[0][0] = grid[0][0]
-
-        def solve(i, j):
-            if i == 0 and j == 0:
-                return memo[i][j]
-            if memo[i][j] == -1:
-                up, left = float('inf'), float('inf')
-                if i > 0:
-                    up = solve(i - 1, j)
-                if j > 0:
-                    left = solve(i, j - 1)
-                memo[i][j] = grid[i][j] + min(up, left)
-            return memo[i][j]
-
-        return solve(m - 1, n - 1)
-    
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    memo[i][j] = grid[i][j]
+                elif i == 0:
+                    memo[i][j] = grid[i][j] + memo[i][j - 1]
+                elif j == 0:
+                    memo[i][j] = grid[i][j] + memo[i - 1][j]
+                else:
+                    memo[i][j] = grid[i][j] + min(memo[i][j - 1], memo[i - 1][j])
+        return memo[-1][-1]
+                    
 
 print(Solution().minPathSum(grid = [[1,2,3],[4,5,6]]))
