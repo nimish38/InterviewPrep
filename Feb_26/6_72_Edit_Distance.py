5e439f6ea1245176f37562553c77a186416debb2
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-06 21:59:41 +0530

    Dynamic programming with memoization

--- Feb_26/6_72_Edit_Distance.py
+++ Feb_26/6_72_Edit_Distance.py
@@ -1,17 +1,21 @@
 class Solution(object):
     def minDistance(self, word1, word2):
         m, n = len(word1), len(word2)
+        memo = [[-1] * n for _ in range(m)]
         def solve(i, j):
             if i == m:
                 return n - j
             if j == n:
                 return m - i
-            if word1[i] == word2[j]:
-                return solve(i + 1, j + 1)
-            ins = solve(i, j + 1)
-            dele = solve(i + 1, j)
-            rep = solve(i + 1, j + 1)
-            return 1 + min(ins, dele, rep)
+            if memo[i][j] == -1:
+                if word1[i] == word2[j]:
+                    memo[i][j] = solve(i + 1, j + 1)
+                else:
+                    ins = solve(i, j + 1)
+                    dele = solve(i + 1, j)
+                    rep = solve(i + 1, j + 1)
+                    memo[i][j] = 1 + min(ins, dele, rep)
+            return memo[i][j]
         return solve(0, 0)
 
 print(Solution().minDistance(word1 = "intention", word2 = "execution"))
