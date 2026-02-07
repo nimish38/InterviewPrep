8c84d152dfdcf261f5f9ec5e9bcbd917d3d33b29
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-07 23:13:16 +0530

    debug and fix sample tests

--- Feb_26/7_73_Set_Matrix_Zeroes.py
+++ Feb_26/7_73_Set_Matrix_Zeroes.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def setZeroes(self, matrix):
-        rowZero, colZero, n = False, False, len(matrix)
-        for i in range(n):
+        rowZero, colZero, m, n = False, False, len(matrix), len(matrix[0])
+        for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
                     matrix[i][0] = matrix[0][j] = 0
@@ -9,13 +9,16 @@ class Solution(object):
                         rowZero = True
                     if j == 0:
                         colZero = True
-        for i in range(1, n):
+        for i in range(1, m):
             for j in range(1, n):
                 if matrix[0][j] == 0 or matrix[i][0] == 0:
                     matrix[i][j] = 0
         if rowZero:
             matrix[0] = [0] * n
         if colZero:
-            for _ in range(n):
+            for _ in range(m):
                 matrix[_][0] = 0
         return matrix
+
+
+print(Solution().setZeroes(matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]))
