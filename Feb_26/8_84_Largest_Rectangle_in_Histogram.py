12f404a8d7c78f40a1ddfbc60b589f28d2b4edd5
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-08 13:18:38 +0530

    find nse and pse using monotonic stack, clear sample tests

--- Feb_26/8_84_Largest_Rectangle_in_Histogram.py
+++ Feb_26/8_84_Largest_Rectangle_in_Histogram.py
@@ -2,14 +2,12 @@ class Solution(object):
     def largestRectangleArea(self, heights):
         n, res = len(heights), 0
         pse, nse, st = [-1] * n, [n] * n, []
-
         for i in range(n):
             while st and st[-1][0] >= heights[i]:
                 st.pop()
             if st:
               pse[i] = st[-1][1]
             st.append((heights[i], i))
-
         st.clear()
         for i in range(n - 1, -1, -1):
             while st and st[-1][0] >= heights[i]:
@@ -17,9 +15,11 @@ class Solution(object):
             if st:
                 nse[i] = st[-1][1]
             st.append((heights[i], i))
-
         for i in range(n):
             val = heights[i] * (nse[i] - pse[i] - 1)
             if val > res:
                 res = val
         return res
+
+
+print(Solution().largestRectangleArea(heights = [2,1,5,6,2,3]))
