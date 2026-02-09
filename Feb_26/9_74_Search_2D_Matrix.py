ea3c39f09afc460f7c986ed26f7da6817eb51802
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-09 17:59:08 +0530

    minor debug and fix

--- Feb_26/9_74_Search_2D_Matrix.py
+++ Feb_26/9_74_Search_2D_Matrix.py
@@ -3,8 +3,10 @@ class Solution(object):
         m, n = len(matrix), len(matrix[0])
         def binSearchRow():
             u, d = 0, m - 1
-            mid, row = (u + d) // 2, -1
-            while u < d:
+            mid = (u + d) // 2
+            row = mid
+            while u <= d:
+                mid = (u + d) // 2
                 if matrix[mid][0] == target:
                     return mid
                 elif matrix[mid][0] > target:
@@ -16,7 +18,7 @@ class Solution(object):
 
         def binSearchCol(row):
             l, r = 0, n - 1
-            while l < r:
+            while l <= r:
                 mid = (l + r) // 2
                 if matrix[row][mid] == target:
                     return True
@@ -28,3 +30,6 @@ class Solution(object):
 
         row = binSearchRow()
         return binSearchCol(row)
+
+
+print(Solution().searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3))
