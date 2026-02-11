c3f072f69b7d0e42138665b8152bcfc0a2e070d7
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-11 22:49:10 +0530

    sort array and then skip duplicates only for not take case

--- Feb_26/11_90_Subsets_II.py
+++ Feb_26/11_90_Subsets_II.py
@@ -1,16 +1,18 @@
 class Solution(object):
     def subsetsWithDup(self, nums):
         n, res = len(nums), []
+        nums.sort()
         def solve(ind, combo):
             if ind == n:
-                x = sorted(list(combo))
-                if x not in res:
-                    res.append(x)
+                res.append(list(combo))
                 return
             combo.append(nums[ind])
             solve(ind + 1, combo)
             combo.pop()
-            solve(ind + 1, combo)
+            ind += 1
+            while ind < n and nums[ind] == nums[ind - 1]:
+                ind += 1
+            solve(ind, combo)
         solve(0, [])
         return res
 
