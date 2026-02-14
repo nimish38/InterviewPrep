c6861a20486d0ba0a11c91b4a9466aa0d686582c
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-14 19:32:41 +0530

    use a separate array for child nodes and use LIFO to reverse direction

--- Feb_26/14_103_BT_Zigzag_Level_Traversal.py
+++ Feb_26/14_103_BT_Zigzag_Level_Traversal.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -11,29 +8,30 @@ class Solution(object):
     def zigzagLevelOrder(self, root):
         if not root:
             return []
-        st, res, leftToRight = deque([root]), [], True
+        st, res, kids, leftToRight = [root], [], [],  True
 
         def checkAndAdd(x):
             if x:
-                st.append(x)
+                kids.append(x)
 
         while st:
             n, lvl = len(st), []
             for _ in range(n):
-                node = st.popleft()
+                node = st.pop()
                 lvl.append(node.val)
                 if leftToRight:
-                    checkAndAdd(node.right)
                     checkAndAdd(node.left)
+                    checkAndAdd(node.right)
                 else:
-                    checkAndAdd(node.left)
                     checkAndAdd(node.right)
+                    checkAndAdd(node.left)
             res.append(lvl)
             leftToRight = not leftToRight
+            st, kids = kids, []
         return res
 
 
-a, b, c, d, e = TreeNode(3), TreeNode(9), TreeNode(20), TreeNode(15), TreeNode(7)
+a, b, c, d, e = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5)
 a.left, a.right = b, c
-c.left, c.right = d, e
+b.left, c.right = d, e
 print(Solution().zigzagLevelOrder(a))
