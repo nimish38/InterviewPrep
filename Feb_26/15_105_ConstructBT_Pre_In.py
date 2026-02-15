7ec4cda93c72fd416a321f45beb587eb1a6660e4
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-15 14:04:31 +0530

    using index and static attribute for inorder

--- Feb_26/15_105_ConstructBT_Pre_In.py
+++ Feb_26/15_105_ConstructBT_Pre_In.py
@@ -6,11 +6,19 @@ class TreeNode(object):
 
 class Solution(object):
     def buildTree(self, preorder, inorder):
-        if not preorder:
-            return None
-        val = preorder[0]
-        ind = inorder.index(val)
-        node = TreeNode(val)
-        node.left = self.buildTree(preorder[1: ind + 1], inorder[:ind])
-        node.right = self.buildTree(preorder[ind + 1: ], inorder[ind + 1:])
-        return node
+        self.pre = 0
+        def build(inStart, inEnd):
+            if inStart > inEnd:
+                return None
+            val = preorder[self.pre]
+            self.pre += 1
+            ind = inorder.index(val)
+            node = TreeNode(val)
+            node.left = build(inStart, ind - 1)
+            node.right = build(ind + 1, inEnd)
+            return node
+        return build(0, len(inorder) - 1)
+
+
+x = Solution().buildTree(preorder = [3,9,20,15,7], inorder = [9,3,15,20,7])
+print(x)
