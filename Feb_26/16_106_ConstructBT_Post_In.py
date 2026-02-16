611cf55f7591c00da0e2f477893e265b1db9507a
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-16 17:39:01 +0530

    attempt to build tree with reverse postorder

---
+++ Feb_26/16_106_ConstructBT_Post_In.py
@@ -0,0 +1,20 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        self.post = len(postorder) - 1
+        def build(inStart, inEnd):
+            if inStart > inEnd:
+                return None
+            val = postorder[self.post]
+            self.post -= 1
+            node = TreeNode(val)
+            ind = inorder.index(val)
+            node.right = build(ind + 1, inEnd)
+            node.left = build(inStart, ind - 1)
+            return node
+        return build(0, len(inorder) - 1)
