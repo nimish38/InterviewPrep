97e0ebd402320ae36aa5cf66bec4392b71c4003e
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-18 22:29:28 +0530

    attempt to build recursively by mid node

---
+++ Feb_26/18_108_Convert_Sorted_Array_BST.py
@@ -0,0 +1,18 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        def build(l, r):
+            if l > r:
+                return None
+            mid = (l + r) // 2
+            node = TreeNode(nums[mid])
+            node.left = build(l, mid - 1)
+            node.right = build(mid + 1, r)
+            return node
+        return build(0, len(nums) - 1)
