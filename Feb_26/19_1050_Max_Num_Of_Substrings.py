e430475d53a06b515596b37f6a81f93cf41ec363
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-20 00:13:47 +0530

    build start and end indexes for each char and then expand to find substrings that satisfy both rules

--- Feb_26/19_1050_Max_Num_Of_Substrings.py
+++ Feb_26/19_1050_Max_Num_Of_Substrings.py
@@ -3,19 +3,24 @@ class Solution(object):
         chars, res = {}, []
         for i in range(len(s)):
             if s[i] not in chars:
-                chars[s[i]] = 1
+                chars[s[i]] = [i, i]
             else:
-                if chars[s[i]] == -1:
-                    continue
-                elif s[i - 1] == s[i]:
-                    chars[s[i]] += 1
-                else:
-                    chars[s[i]] = -1
+                chars[s[i]][1] = i
 
         for key in chars:
-            if chars[key] != -1:
-                res.append(key * chars[key])
-        return res
+            start, end = chars[key]
+            st = [(start, end)]
+            while st:
+                curr_st, curr_ed = st.pop()
+                for i in range(curr_st, curr_ed + 1):
+                    new_st, new_ed = chars[s[i]]
+                    if new_st < curr_st:
+                        st.append((new_st, curr_st - 1))
+                        start = new_st
+                    if new_ed > curr_ed:
+                        st.append((curr_ed + 1, new_ed))
+                        end = new_ed
+            chars[key] = [start, end]
 
 
-print(Solution().maxNumOfSubstrings(s = "adefaddaccc"))
+print(Solution().maxNumOfSubstrings(s = "abbaccd"))
