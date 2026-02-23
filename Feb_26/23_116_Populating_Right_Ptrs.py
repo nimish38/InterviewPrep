0ff41b8877a0924f1eceb4f6a77dc670271c2524
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-23 22:11:34 +0530

    test with sample case

--- Feb_26/23_116_Populating_Right_Ptrs.py
+++ Feb_26/23_116_Populating_Right_Ptrs.py
@@ -23,3 +23,10 @@ class Solution(object):
                     kids.append(node.right)
             st, kids = kids, []
         return root
+
+
+a, b, c, d, e, f, g = Node(1), Node(2), Node(3), Node(4), Node(5), Node(6), Node(7)
+a.left, a.right = b, c
+b.left, b.right, c.left, c.right = d, e, f, g
+x = Solution().connect(a)
+print(x)
