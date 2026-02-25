da26d0fecb23c80ab73dd77f54679cdc5c7ee779
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-25 21:27:23 +0530

    put in else for optimal

--- Feb_26/25_114_Best_Time_To_BNS_Stock.py
+++ Feb_26/25_114_Best_Time_To_BNS_Stock.py
@@ -2,10 +2,10 @@ class Solution(object):
     def maxProfit(self, prices):
         best, buy = 0, prices[0]
         for i in range(1, len(prices)):
-            if prices[i] - buy > best:
-                best = prices[i] - buy
             if prices[i] < buy:
                 buy = prices[i]
+            elif prices[i] - buy > best:
+                best = prices[i] - buy
         return best
 
 
