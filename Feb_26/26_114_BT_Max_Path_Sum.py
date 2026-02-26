2c10e73abde2520d34e70d705bccc071f29b5f6c
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-26 21:15:03 +0530

    Fix edge case

--- Feb_26/26_114_BT_Max_Path_Sum.py
+++ Feb_26/26_114_BT_Max_Path_Sum.py
@@ -13,12 +13,13 @@ class Solution(object):
             left, right = get_max_path(node.left), get_max_path(node.right)
             value = left + right + node.val
             self.best = max(self.best, node.val, value, node.val + left, node.val + right)
-            return node.val + max(left, right)
+            return max(node.val, node.val + max(left, right))
         get_max_path(root)
         return self.best
     
 
-a, b, c, d, e = TreeNode(-10), TreeNode(9), TreeNode(20), TreeNode(15), TreeNode(7), 
-# a.left, a.right = b, c
-# c.left, c.right = d, e
+a, b, c, d, e, p, q, r, s = TreeNode(9), TreeNode(6), TreeNode(-3), TreeNode(-6), TreeNode(2), TreeNode(2),TreeNode(-6),TreeNode(-6),TreeNode(-6)
+a.left, a.right = b, c
+c.left, c.right = d, e
+e.left, p.left, p.right, q.left = p, q, r, s
 print(Solution().maxPathSum(a))
