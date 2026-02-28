be1c4f78980f11279568514c819999221c8f63b2
nimish38 <nimish.v.shinde60@gmail.com>
2026-02-28 15:42:20 +0530

    Backtracking with template

---
+++ Feb_26/28_131_Palindrome_Partition.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def partition(self, s):
+        n, res = len(s), []
+        def solve(ind, curr):
+            if ind >= n:
+                res.append(curr.copy())
+                return
+            for i in range(ind + 1, n + 1):
+                partition = s[ind: i]
+                if partition == partition[::-1]:
+                    curr.append(partition)
+                    solve(i, curr)
+                    curr.pop()
+        solve(0, [])
+        return res
