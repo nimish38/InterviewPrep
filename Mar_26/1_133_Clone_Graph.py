e5f457ed18df8fcd97111b70b40ee62cf9a75297
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-01 21:25:33 +0530

    debug and fix

--- Mar_26/1_133_Clone_Graph.py
+++ Mar_26/1_133_Clone_Graph.py
@@ -14,8 +14,19 @@ class Solution(object):
             for nei in root.neighbors:
                 if nei not in mapping:
                     dfs(nei)
-        for node in mapping:
-            for nei in node.neighbors:
-                mapping[node].neighbors.append(mapping[nei])
         dfs(node)
+        for nodes in mapping:
+            for nei in nodes.neighbors:
+                mapping[nodes].neighbors.append(mapping[nei])
         return mapping[node]
+
+    def buildGraph(self, nodes, adj):
+        for i in range(1, len(nodes)):
+            for neig in adj[i - 1]:
+                nodes[i].neighbors.append(nodes[neig])
+        return nodes[1]
+
+a, b, c, d = Node(1), Node(2), Node(3), Node(4)
+y =Solution().buildGraph([1, a, b, c, d], [[2,4],[1,3],[2,4],[1,3]])
+x = Solution().cloneGraph(y)
+print(x.val)
