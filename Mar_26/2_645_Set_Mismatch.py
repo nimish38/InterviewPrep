f3fbd9df3c8e07c74f215bd95f284f1e261b7edf
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-02 21:49:15 +0530

    mathematical

--- Mar_26/2_645_Set_Mismatch.py
+++ Mar_26/2_645_Set_Mismatch.py
@@ -1,13 +1,9 @@
 class Solution(object):
     def findErrorNums(self, nums):
-        vals, dup, mis = {}, -1, -1
-        for num in nums:
-            if num in vals:
-                dup = num
-            vals[num] = 1
-        for i in range(1, len(nums) + 1):
-            if i not in vals:
-                return [dup, i]
-        return None
+        n = len(nums)
+        a = sum(nums)
+        b = sum(set(nums))
+        s = n * (n + 1) // 2
+        return [a - b , s - b]
 
 print(Solution().findErrorNums(nums = [1,2,2,4]))
