5de181774bb6ab586b950c36354b8385396bcb78
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-03 21:34:45 +0530

    Level Order Traversal with each values as string

---
+++ Mar_26/3_101_Symmetric_Tree.py
@@ -0,0 +1,29 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        st = [root]
+        while st:
+            level = '#'
+            for _ in range(len(st)):
+                node = st.pop(0)
+                if node.left:
+                    st.append(node.left)
+                    level += node.left.val + '#'
+                else:
+                    level += 'N#'
+                if node.right:
+                    st.append(node.right)
+                    level += node.right.val + '#'
+                else:
+                    level += 'N#'
+            if level != level[::-1]:
+                return False
+        return True
+                
