3dccfe2d55789a2ce17232035e7517ddeb3e3cdf
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-05 01:27:13 +0530

    using recursive method with two pointers

--- Mar_26/4_1547_Min_Cost_Cut_Stick.py
+++ Mar_26/4_1547_Min_Cost_Cut_Stick.py
@@ -2,15 +2,14 @@ class Solution(object):
     def minCost(self, n, cuts):
         cuts.extend([0, n])
         cuts.sort()
-        def solve(arr):
-            if len(arr) <= 2:
+        def solve(l, r):
+            if r - l < 2:
                 return 0
             val = float('inf')
-            for i in range(1, len(arr) - 1):
-                curr = solve(arr[: i + 1]) + solve(arr[i:]) + (arr[-1] - arr[0])
+            for i in range(l + 1, r):
+                curr = solve(l, i) + solve(i, r) + (cuts[r] - cuts[l])
                 val = min(val, curr)
             return val
-        return n + solve(cuts)
+        return solve(0, len(cuts) - 1)
 
-
-print(Solution().minCost(n = 7, cuts = [1,3,4,5]))
+print(Solution().minCost(n = 9, cuts = [5,6,1,4,2]))
