adddf07122f142c34f94c4135e33093d7c9f499c
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-06 21:18:34 +0530

    test with sample cases

--- Mar_26/6_141_Linked_List_Cycle.py
+++ Mar_26/6_141_Linked_List_Cycle.py
@@ -16,3 +16,7 @@ class Solution(object):
             if fast.next:
                 fast = fast.next
         return False
+
+a ,b ,c, d = ListNode(1), ListNode(2), ListNode(3), ListNode(4)
+a.next, b.next, c.next, d.next = b, c, d, b
+print(Solution().hasCycle(a))
