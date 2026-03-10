0a799072c962261d1a5d4c0750bbd186d0d453fa
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-10 22:32:35 +0530

    Recursive solution

---
+++ Mar_26/9_144_BT_Preorder.py
@@ -0,0 +1,16 @@
+def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        res = []
+        def preorder(node):
+            if not node:
+                return
+            res.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+        preorder(root)
+        return res
