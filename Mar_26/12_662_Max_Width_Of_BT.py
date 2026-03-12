7396498cb743045bbd41fca432c98c221d495724
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-12 23:41:43 +0530

    debug and fix

--- Mar_26/12_662_Max_Width_Of_BT.py
+++ Mar_26/12_662_Max_Width_Of_BT.py
@@ -6,24 +6,30 @@ class TreeNode(object):
 
 class Solution(object):
     def widthOfBinaryTree(self, root):
-        st, isRoot, left, right, cnt, end, best = [root], True, False, False, -1, -1, -1
-        while st and isRoot or ( left != -1 and right != -1 ):
-            left, right = False, False
+        st, isRoot, left, right, cnt, end, best = [root], True, True, True, -1, -1, -1
+        while st and isRoot or ( left and right ):
+            if not isRoot:
+                left, right = False, False
             for _ in range(len(st)):
-                node = st.pop()
+                node = st.pop(0)
                 if node:
                     if not left:
                         left = True
                         cnt = 1
                     else:
                         cnt += 1
-                        if not right:
-                            right = True
-                            end = cnt
+                        right = True
+                        end = cnt
                     st.append(node.left)
                     st.append(node.right)
                 else:
+                    if left:
+                        cnt += 1
                     st.extend([None, None])
             best = max(best, end)
-            cnt, end= -1, -1
+            cnt, end, isRoot = -1, -1, False
         return best
+    
+a, b, c, d, e, f = TreeNode(1), TreeNode(2), TreeNode(4), TreeNode(3), TreeNode(5), TreeNode(6)
+a.left, a.right, b.left, b.right, c.right = b, c, d, e, f
+print(Solution().widthOfBinaryTree(a))
