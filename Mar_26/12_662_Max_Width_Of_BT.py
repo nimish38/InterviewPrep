b240e0da088ef2966b9fd66bfadea86a916197e4
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-13 00:12:56 +0530

    optimal with Binary tree formaula

--- Mar_26/12_662_Max_Width_Of_BT.py
+++ Mar_26/12_662_Max_Width_Of_BT.py
@@ -1,34 +1,23 @@
+from collections import deque
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
 class Solution(object):
     def widthOfBinaryTree(self, root):
-        st, isRoot, left, right, cnt, end, best = [root], True, True, True, -1, -1, -1
-        while st and isRoot or ( left and right ):
-            if not isRoot:
-                left, right = False, False
+        st, best, width = deque([(root, 0)]), 1, -1
+        while st:
             for _ in range(len(st)):
-                node = st.pop(0)
-                if node:
-                    if not left:
-                        left = True
-                        cnt = 1
-                    else:
-                        cnt += 1
-                        right = True
-                        end = cnt
-                    st.append(node.left)
-                    st.append(node.right)
-                else:
-                    if left:
-                        cnt += 1
-                    st.extend([None, None])
-            best = max(best, end)
-            cnt, end, isRoot = -1, -1, False
-        return best
+                node, ind = st.popleft()
+                if node.left:
+                    st.append((node.left, ind * 2))
+                if node.right:
+                    st.append((node.right, 1 + (ind * 2)))
+            if len(st) >= 2:
+                width = 1 - st[0][1] + st[-1][1]
+            best = max(width, best)
+        return best              
     
 a, b, c, d, e, f = TreeNode(1), TreeNode(2), TreeNode(4), TreeNode(3), TreeNode(5), TreeNode(6)
 a.left, a.right, b.left, b.right, c.right = b, c, d, e, f
