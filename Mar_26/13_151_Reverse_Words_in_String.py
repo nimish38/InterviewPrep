72a0eb9e4923dcdf7ca76416ce759d50d197dd39
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-13 22:17:53 +0530

    using self built logic

--- Mar_26/13_151_Reverse_Words_in_String.py
+++ Mar_26/13_151_Reverse_Words_in_String.py
@@ -1,6 +1,20 @@
 class Solution(object):
     def reverseWords(self, s):
-        s = s.split()
-        return ' '.join(s[::-1])
+        words, i, res = [], 0, ''
+        while s[i] == ' ':
+            i += 1
+        while i < len(s):
+            letter = ''
+            while i < len(s) and s[i] != ' ':
+                letter += s[i]
+                i += 1
+            words.append(letter)
+            while i < len(s) and s[i] == ' ':
+                i += 1
+        for word in range(len(words) - 1, -1, -1):
+            res += words[word]
+            if word != 0:
+                res += ' '
+        return res
     
 print(Solution().reverseWords(s = "  hello world  "))
