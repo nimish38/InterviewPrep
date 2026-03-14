f1283b342b73ba989119e16e4d13ecd2178f2dfc
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-14 15:17:33 +0530

    brute force, but reducing one loop

--- Mar_26/14_152_Max_Prod_Subarr.py
+++ Mar_26/14_152_Max_Prod_Subarr.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def maxProduct(self, nums):
-        n, best = len(nums), max(nums)
-        for i in range(1, n + 1):
-            for j in range(n - i + 1):
-                curr = 1
-                for k in nums[j: j + i]:
-                    curr *= k
-                    if k == 0:
-                        break
+        n, best = len(nums), float('-inf')
+        for i in range(n):
+            curr = 1
+            for j in range(i, n):
+                curr *= nums[j]
+                if nums[j] == 0:
+                    break
                 if curr > best:
                     best = curr
         return best
