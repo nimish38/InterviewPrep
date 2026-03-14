39aefadab15fac784de23f967811efb9751d9b50
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-14 15:01:22 +0530

    brute force with growing window

---
+++ Mar_26/14_152_Max_Prod_Subarr.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        n, best = len(nums), 0
+        for i in range(1, n + 1):
+            for j in range(n - i + 1):
+                curr = 1
+                for k in nums[j: j + i]:
+                    curr *= k
+                    if k == 0:
+                        break
+                if curr > best:
+                    best = curr
+        return best
+
+print(Solution().maxProduct(nums = [2,3,-2,4]))
