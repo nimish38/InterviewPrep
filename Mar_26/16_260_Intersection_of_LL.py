ae4d44b3d30b919a5a760a734c11f33e91d867e8
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-16 21:46:43 +0530

    move one pointer by diff steps ahead

--- Mar_26/16_260_Intersection_of_LL.py
+++ Mar_26/16_260_Intersection_of_LL.py
@@ -5,17 +5,29 @@ class ListNode(object):
 
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
-        def reverse(head):
-            p, q = head, head.next
-            while q:
-                r = q.next
-                q.next = p
-                p, q = q, r
-            head.next = None
-            return p 
-        headA, headB, inter = reverse(headA), reverse(headB), None
-        while headB == headA:
-            inter = headA
+        def findLength(head):
+            p, cnt = head, 0
+            while p:
+                cnt += 1
+                p = p.next
+            return cnt
+        def moveAhead(head, val):
+            for _ in range(val):
+                head = head.next
+            return head
+        l1, l2 = findLength(headA), findLength(headB)
+        diff = l1 - l2
+        if diff > 0:
+            headA = moveAhead(headA, diff)
+        else:
+            headB = moveAhead(headB, abs(diff))
+        while headA and headB and headA != headB:
             headA = headA.next
             headB = headB.next
-        return inter
+        return headA
+
+
+    
+a, b, c, d, e = ListNode(1), ListNode(2), ListNode(3), ListNode(4), ListNode(5)
+a.next, b.next, d.next, e.next = b, c, e, c
+print(Solution().getIntersectionNode(a, d).val)
