289210e741632925a9a8d231a5af593c3abe9c3f
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-18 21:29:45 +0530

    brute force with n time

--- Mar_26/18_169_Majority_Element.py
+++ Mar_26/18_169_Majority_Element.py
@@ -1,14 +1,6 @@
-from collections import defaultdict
-
-
 class Solution(object):
     def majorityElement(self, nums):
-        maps, n = defaultdict(int), len(nums)
-        for num in nums:
-            maps[num] += 1
-        for key in maps:
-            if maps[key] > n // 2:
-                return key
-        return -1
+        nums.sort()
+        return nums[len(nums) // 2]
 
 print(Solution().majorityElement(nums = [2,2,1,1,1,2,2]))
