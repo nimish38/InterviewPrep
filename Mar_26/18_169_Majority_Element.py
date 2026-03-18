20d04eb0c97f3fbc7ff55d9e2e5488f7bb45049f
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-18 22:35:23 +0530

    optimal with keeping count and elem

--- Mar_26/18_169_Majority_Element.py
+++ Mar_26/18_169_Majority_Element.py
@@ -1,6 +1,14 @@
 class Solution(object):
     def majorityElement(self, nums):
-        nums.sort()
-        return nums[len(nums) // 2]
+        major, cnt = nums[0], 1
+        for i in range(1, len(nums)):
+            if nums[i] == major:
+                cnt += 1
+            else:
+                if cnt == 0:
+                    major, cnt = nums[i], 1
+                else:
+                    cnt -= 1
+        return major
 
 print(Solution().majorityElement(nums = [2,2,1,1,1,2,2]))
