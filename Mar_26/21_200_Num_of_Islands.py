519b5887fbf352f2eb8dcfbb0185a9c04154d4e2
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-21 00:36:19 +0530

    iterative dfs

--- Mar_26/21_200_Num_of_Islands.py
+++ Mar_26/21_200_Num_of_Islands.py
@@ -1,15 +1,17 @@
 class Solution(object):
     def numIslands(self, grid):
         cnt, m, n = 0, len(grid), len(grid[0])
-
         def dfs(i, j):
+            st = [(i, j)]
             grid[i][j] = 'v'
             nei = [(1, 0),(-1, 0), (0, 1), (0, -1)]
-            for x, y in nei:
-                dx, dy = i + x, j + y
-                if 0 < dx < m and 0 < dy < n and grid[dx][dy] == '1':
-                    dfs(dx, dy)
-
+            while st:
+                p, q = st.pop()
+                for x, y in nei:
+                    dx, dy = p + x, q + y
+                    if 0 <= dx < m and 0 <= dy < n and grid[dx][dy] == '1':
+                        st.append((dx, dy))
+                        grid[dx][dy] = 'v'
         for k in range(m):
             for l in range(n):
                 if grid[k][l] == '1':
