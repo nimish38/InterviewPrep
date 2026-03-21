4f1a0ff2fb3194938baf51bba2e04aa8ebde03fd
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-21 14:03:54 +0530

    Mathematical

--- Mar_26/21_686_Repeating_Str_Match.py
+++ Mar_26/21_686_Repeating_Str_Match.py
@@ -1,28 +1,12 @@
-class Solution(object):
+class Solution:
     def repeatedStringMatch(self, a, b):
-        if len(set(b) - set(a)) > 0:
+        if set(b) - set(a):
             return -1
-        i, j, m, n = 0, 0, len(a), len(b)
-        def checkRematch(x, y):
-            cnt = 1
-            while y < n and b[y] == a[x]:
-                y += 1
-                if x == m - 1 and y < n:
-                    cnt += 1
-                x = (x + 1) % m
-            if y == n:
-                return cnt
-            return -1
-
-        while i < m:
-            while i < m and a[i] != b[j]:
-                i += 1
-            if i < m:
-                x = checkRematch(i, j)
-                if x != -1:
-                    return x
-                j, i = 0, i + 1
+        n, m = len(a), len(b)
+        k = (m + n - 1) // n
+        s = a * k
+        if b in s:
+            return k
+        if b in s + a:
+            return k + 1
         return -1
-
-
-print(Solution().repeatedStringMatch(a = "abcabcabcabc", b = "abac"))
