8dfc3d20c86e9b6d287b8675277966b600ed7d9c
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-24 21:42:34 +0530

    Implement prefix and unit test

--- Mar_26/24_208_Implement_Trie.py
+++ Mar_26/24_208_Implement_Trie.py
@@ -1,8 +1,6 @@
 class Trie(object):
-
     def __init__(self):
         self.trie = {}
-
     def insert(self, word):
         curr = self.trie
         for c in word:
@@ -10,7 +8,6 @@ class Trie(object):
                 curr[c] = {}
             curr = curr[c]
         curr['*'] = word
-
     def search(self, word):
         curr = self.trie
         for c in word:
@@ -20,5 +17,18 @@ class Trie(object):
         if '*' in curr:
             return True
         return False
-
     def startsWith(self, prefix):
+        curr = self.trie
+        for c in prefix:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return True
+
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple"))
+print(trie.search("app"))
+print(trie.startsWith("app"))
+trie.insert("app")
+print(trie.search("app"))
