f1e0116abf3b185c99210ca38fac1a6f7461a24e
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-25 22:20:28 +0530

    Using heap

--- Mar_26/25_215_Kth_Largest_Element.py
+++ Mar_26/25_215_Kth_Largest_Element.py
@@ -1,3 +1,10 @@
+import heapq
+
 class Solution(object):
     def findKthLargest(self, nums, k):
-        return sorted(nums)[len(nums) - k]
+        heapq.heapify(nums)
+        for _ in range(k - 1):
+            heapq.heappop(nums)
+        return heapq.heappop(nums)
+
+print(Solution().findKthLargest())
