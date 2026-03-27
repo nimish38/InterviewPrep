e25b33a727b3db685593871194d31ef9f38bd731
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-27 22:11:13 +0530

    bottom up recursive dp with memoization

--- Mar_26/27_1143_Longest_Com_Subseq.py
+++ Mar_26/27_1143_Longest_Com_Subseq.py
@@ -1,10 +1,16 @@
 class Solution(object):
     def longestCommonSubsequence(self, text1, text2):
         m, n = len(text1), len(text2)
+        memo = [[-1] * n for _ in range(m)]
         def solve(i, j):
             if i == m or j == n:
                 return 0
-            if text1[i] == text2[j]:
-                return 1 + solve(i + 1, j + 1)
-            return max(solve(i + 1, j), solve(i, j + 1))
+            if memo[i][j] == -1:
+                if text1[i] == text2[j]:
+                    memo[i][j] = 1 + solve(i + 1, j + 1)
+                else:
+                    memo[i][j] = max(solve(i + 1, j), solve(i, j + 1))
+            return memo[i][j]
         return solve(0, 0)
+
+print(Solution().longestCommonSubsequence(text1 = "abc", text2 = "def" ))
