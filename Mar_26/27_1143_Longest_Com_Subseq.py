44f157d193828b907fa2d4e92391c46f904ea6f0
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-27 22:24:30 +0530

    top down dp

--- Mar_26/27_1143_Longest_Com_Subseq.py
+++ Mar_26/27_1143_Longest_Com_Subseq.py
@@ -1,16 +1,13 @@
 class Solution(object):
     def longestCommonSubsequence(self, text1, text2):
         m, n = len(text1), len(text2)
-        memo = [[-1] * n for _ in range(m)]
-        def solve(i, j):
-            if i == m or j == n:
-                return 0
-            if memo[i][j] == -1:
-                if text1[i] == text2[j]:
-                    memo[i][j] = 1 + solve(i + 1, j + 1)
+        memo = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    memo[i][j] = 1 + memo[i - 1][j - 1]
                 else:
-                    memo[i][j] = max(solve(i + 1, j), solve(i, j + 1))
-            return memo[i][j]
-        return solve(0, 0)
+                    memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
+        return memo[-1][-1]
 
-print(Solution().longestCommonSubsequence(text1 = "abc", text2 = "def" ))
+print(Solution().longestCommonSubsequence(text1 = "pmjghexybyrgzczy", text2 = "hafcdqbgncrcbihkd" ))
