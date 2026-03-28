7ee7b5dd7666d900718d7dc235b00896af78f4a5
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-28 11:30:35 +0530

    Moore Voting Algo

--- Mar_26/28_229_Majority_Element_II.py
+++ Mar_26/28_229_Majority_Element_II.py
@@ -1,12 +1,32 @@
-from collections import defaultdict
-
-
 class Solution(object):
     def majorityElement(self, nums):
-        target, maps, res = len(nums) // 3, defaultdict(int), []
+        target, top1, top2, res = len(nums) // 3, [None, 0], [None, 0], []
         for num in nums:
-            maps[num] += 1
-        for key in maps:
-            if maps[key] > target:
-                res.append(key)
+            if top1[0] == None:
+                top1 = [num, 1]
+            elif num != top1[0] and top2[0] == None:
+                top2 = [num, 1]
+            elif num == top1[0]:
+                top1[1] += 1
+            elif num == top2[0]:
+                top2[1] += 1
+            else:
+                top2[1] -= 1
+                if top2[1] == -1:
+                    top2 = [num, 1]
+            if top2[1] > top1[1]:
+                top1, top2 = top2, top1
+
+        top1[1], top2[1] = 0, 0
+        for num in nums:
+            if num == top1[0]:
+                top1[1] += 1
+            if num == top2[0]:
+                top2[1] += 1
+        if top1[1] > target:
+            res.append(top1[0])
+        if top2[1] > target:
+            res.append(top2[0])
         return res
+
+print(Solution().majorityElement(nums=[1, 2]))
