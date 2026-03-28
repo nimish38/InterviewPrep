1ef91ba448de3e27bbe1f611fce15e02191ae589
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-28 11:36:16 +0530

    Remove From Top1 also when doesnt match

--- Mar_26/28_229_Majority_Element_II.py
+++ Mar_26/28_229_Majority_Element_II.py
@@ -1,32 +1,25 @@
 class Solution(object):
     def majorityElement(self, nums):
-        target, top1, top2, res = len(nums) // 3, [None, 0], [None, 0], []
-        for num in nums:
-            if top1[0] == None:
-                top1 = [num, 1]
-            elif num != top1[0] and top2[0] == None:
-                top2 = [num, 1]
-            elif num == top1[0]:
-                top1[1] += 1
-            elif num == top2[0]:
-                top2[1] += 1
+        if not nums :
+            return []
+        count1 = count2 = 0
+        candidate1 = candidate2 = None
+        for num in nums :
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1 = num
+                count1= 1
+            elif count2 == 0:
+                candidate2 = num
+                count2 = 1
             else:
-                top2[1] -= 1
-                if top2[1] == -1:
-                    top2 = [num, 1]
-            if top2[1] > top1[1]:
-                top1, top2 = top2, top1
-
-        top1[1], top2[1] = 0, 0
-        for num in nums:
-            if num == top1[0]:
-                top1[1] += 1
-            if num == top2[0]:
-                top2[1] += 1
-        if top1[1] > target:
-            res.append(top1[0])
-        if top2[1] > target:
-            res.append(top2[0])
-        return res
-
-print(Solution().majorityElement(nums=[1, 2]))
+                count1 -= 1
+                count2 -= 1
+        result = []
+        for c in [candidate1, candidate2]:
+            if nums.count(c) > len(nums) // 3:
+                result.append(c)
+        return result
