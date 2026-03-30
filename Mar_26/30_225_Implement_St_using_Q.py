b12b25ece25658386ada6b2cc8262cdd53409225
nimish38 <nimish.v.shinde60@gmail.com>
2026-03-30 22:38:24 +0530

    using single Queue

--- Mar_26/30_225_Implement_St_using_Q.py
+++ Mar_26/30_225_Implement_St_using_Q.py
@@ -1,26 +1,31 @@
 class MyStack(object):
     def __init__(self):
-        self.q1, self.q2 = [], []
+        self.q1, self.cnt = [], 0
 
     def push(self, x):
-        self.q1.append(x)
+        self.q1.append((x, self.cnt))
+        self.cnt += 1
 
     def pop(self):
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.pop(0))
-        x = self.q1.pop(0)
-        while self.q2:
-            self.q1.append(self.q2.pop(0))
-        return x
+        top, last = self.q1.pop(0), -1
+        self.q1.append(top)
+        while True:
+            last = self.q1.pop(0)
+            if self.q1[0][1] != top[1]:
+                self.q1.append(last)
+            else:
+                break
+        return last[0]
 
     def top(self):
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.pop(0))
-        x = self.q1.pop(0)
-        self.q2.append(x)
-        while self.q2:
-            self.q1.append(self.q2.pop(0))
-        return x
+        top, last = self.q1.pop(0), -1
+        self.q1.append(top)
+        while True:
+            last = self.q1.pop(0)
+            self.q1.append(last)
+            if self.q1[0][1] == top[1]:
+                break
+        return last[0]
 
     def empty(self):
         return len(self.q1) == 0
