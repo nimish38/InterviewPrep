5d826254af56753aae228d0d92f0593d7edf2a34
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-02 22:44:54 +0530

    save values in a string

---
+++ Apr_26/2_234_Palindrome_LL.py
@@ -0,0 +1,14 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def isPalindrome(self, head):
+        vals = '#'
+        while head:
+            vals += head.val + '#'
+            head = head.next
+        return vals == vals[::-1]
+
+print(Solution().isPalindrome())
