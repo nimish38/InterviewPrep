aca869dad236c6c625ee40599b594c09fe9ee873
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-02 22:53:29 +0530

    reverse first half

--- Apr_26/2_234_Palindrome_LL.py
+++ Apr_26/2_234_Palindrome_LL.py
@@ -3,13 +3,24 @@ class ListNode(object):
         self.val = val
         self.next = next
 
-class Solution(object):
+class Solution:
     def isPalindrome(self, head):
-        vals = '#'
-        while head:
-            vals += str(head.val) + '#'
-            head = head.next
-        return vals == vals[::-1]
+        prev = None
+        slow = fast = head
+        # Reverse first half while finding middle
+        while fast and fast.next:
+            fast = fast.next.next
+            slow.next, prev, slow = prev, slow, slow.next
+        # Skip middle if odd length
+        if fast:
+            slow = slow.next
+        # Compare
+        while prev:
+            if prev.val != slow.val:
+                return False
+            prev = prev.next
+            slow = slow.next
+        return True
 
 a, b, c, d, e = ListNode(1), ListNode(2), ListNode(3), ListNode(2) ,ListNode(1),
 print(Solution().isPalindrome(a))
