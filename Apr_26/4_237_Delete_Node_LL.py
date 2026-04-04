26d77fd37be3c4037199fe37d336fb4d6f3c7e32
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-04 19:22:42 +0530

    test and fix

--- Apr_26/4_237_Delete_Node_LL.py
+++ Apr_26/4_237_Delete_Node_LL.py
@@ -9,4 +9,10 @@ class Solution(object):
         while super:
             node.val = succ.val
             node, succ, super = succ, super, super.next
+        node.val = succ.val
         node.next = None
+
+a, b, c, d, e = ListNode(1), ListNode(2), ListNode(3), ListNode(4), ListNode(5)
+a.next, b.next, c.next, d.next = b, c, d, e
+Solution().deleteNode(b)
+print(a)
