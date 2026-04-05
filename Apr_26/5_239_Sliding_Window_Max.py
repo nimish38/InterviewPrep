291b8b93ffa4017f31b18d9bf9312781045d7f2f
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-05 18:06:35 +0530

    Using monotonic deque

--- Apr_26/5_239_Sliding_Window_Max.py
+++ Apr_26/5_239_Sliding_Window_Max.py
@@ -1,10 +1,24 @@
+from collections import deque
+
+
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
-        heap, res = nums[: k], [max(nums[: k])]
+        deq, res = deque(), []
+        for i in range(k):
+            while deq and nums[deq[0]] < nums[i]:
+                deq.popleft()
+            deq.append(i)
         for i in range(k, len(nums)):
-            heap.pop(0)
-            heap.append(nums[i])
-            res.append(max(heap))
+            while deq[0] < i - k:
+                deq.popleft()
+            res.append(nums[deq[0]])
+            while deq and nums[deq[0]] < nums[i]:
+                deq.popleft()
+            deq.append(i)
+        while deq[0] < len(nums) - k:
+            deq.popleft()
+        res.append(nums[deq[0]])
         return res
+
 
 print(Solution().maxSlidingWindow(nums = [1,3,-1,-3,5,3,6,7], k = 3))
