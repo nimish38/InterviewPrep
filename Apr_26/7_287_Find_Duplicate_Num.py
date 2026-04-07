c5767975024af11804e455fbaea8dc96f9386feb
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-07 22:54:24 +0530

    Using indexes but modifying array

--- Apr_26/7_287_Find_Duplicate_Num.py
+++ Apr_26/7_287_Find_Duplicate_Num.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def findDuplicate(self, nums):
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
-                return nums[i]
+        for num in nums:
+            x = abs(num)
+            if nums[x] < 0:
+                return x
+            nums[x] = - nums[x]
         return -1
 
 print(Solution().findDuplicate(nums = [1,3,4,2,2]))
