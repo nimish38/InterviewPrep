1f06db448af8629e6226b19262e0e6735699860f
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-07 23:01:23 +0530

    simple maths

--- Apr_26/7_287_Find_Duplicate_Num.py
+++ Apr_26/7_287_Find_Duplicate_Num.py
@@ -1,15 +1,6 @@
 class Solution(object):
     def findDuplicate(self, nums):
-        slow, fast = nums[0], nums[0]
-        slow = nums[slow]
-        fast = nums[nums[fast]]
-        while slow != fast:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-        slow = nums[0]
-        while slow != fast:
-            slow = nums[slow]
-            fast = nums[fast]
-        return slow
+        seen = set(nums)
+        return (sum(nums) - sum(seen)) / (len(nums) - len(seen))
 
 print(Solution().findDuplicate(nums = [1,3,4,2,2]))
