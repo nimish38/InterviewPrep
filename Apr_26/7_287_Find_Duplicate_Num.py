c242b50dd029059594c608275334b3a683e1fd78
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-07 21:48:57 +0530

    Using bruteforce sorting

--- Apr_26/7_287_Find_Duplicate_Num.py
+++ Apr_26/7_287_Find_Duplicate_Num.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def findDuplicate(self, nums):
-        maps = set()
-        for num in nums:
-            if num in maps:
-                return num
-            maps.add(num)
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return nums[i]
         return -1
 
 print(Solution().findDuplicate(nums = [1,3,4,2,2]))
