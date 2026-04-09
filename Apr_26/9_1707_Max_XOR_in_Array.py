2239dfb144c878d52419bee7ac69a4b57850e6b6
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-09 22:19:37 +0530

    sort both arrays and build trie from numbers

--- Apr_26/9_1707_Max_XOR_in_Array.py
+++ Apr_26/9_1707_Max_XOR_in_Array.py
@@ -1,14 +1,43 @@
 class Solution(object):
     def maximizeXor(self, nums, queries):
-        res, curr = [], -1
         nums.sort()
-        for x, m in queries:
-            curr, i = -1, 0
+        offline, res, trie = [], [-1] * len(queries), {}
+        for i in range(len(queries)):
+            x, m = queries[i]
+            offline.append([m, x, i])
+        offline.sort(key=lambda r: r[0])
+
+        def insert(num):
+            curr = trie
+            for i in range(31, -1, -1):
+                bit = (num >> i) & 1
+                if bit not in curr:
+                    curr[bit] = {}
+                curr = curr[bit]
+
+        def getMax(num):
+            curr, maxNum = trie, 0
+            for i in range(31, -1, -1):
+                bit = (num >> i) & 1
+                opp = 1 - bit
+                if opp in curr:
+                    curr = curr[opp]
+                    maxNum = maxNum | (1 << i)
+                else:
+                    curr = curr[bit]
+            return maxNum
+
+        i = 0
+        for m, x, ind in offline:
             while i < len(nums) and nums[i] <= m:
-                curr = max(curr, x ^ nums[i])
-                i += 1
-            res.append(curr)
+                insert(nums[i])
+            val = getMax(x)
+            if val == 0:
+                res[ind] = -1
+            else:
+                res[ind] = val
         return res
+
 
 print(Solution().maximizeXor(nums = [5,2,4,6,6,3], queries = [[12,4],[8,1],[6,3]]))
 
