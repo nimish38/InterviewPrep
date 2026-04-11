c53a30aaab5e7bc4826e0fe3494e7f965e0c19e3
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-11 20:46:11 +0530

    debug and fix

--- Apr_26/11_295_FInd_Median_Stream.py
+++ Apr_26/11_295_FInd_Median_Stream.py
@@ -1,22 +1,27 @@
 import heapq
-
-
 class MedianFinder(object):
-
     def __init__(self):
         self.maxheap, self.minheap = [], []
 
     def addNum(self, num):
-        heapq.heappush(self.maxheap, -num)
-        if len(self.maxheap) - len(self.minheap) > 1:
-            heapq.heappush(self.minheap, -heapq.heappop(self.maxheap))
+        if not self.maxheap or num <= -self.maxheap[0]:
+            heapq.heappush(self.maxheap, -num)
+            if len(self.maxheap) - len(self.minheap) > 1:
+                heapq.heappush(self.minheap, -heapq.heappop(self.maxheap))
+        else:
+            heapq.heappush(self.minheap, num)
+            if len(self.minheap) - len(self.maxheap) > 0:
+                heapq.heappush(self.maxheap, -heapq.heappop(self.minheap))
 
     def findMedian(self):
         if len(self.maxheap) == len(self.minheap):
-            return (self.minheap[0] - self.maxheap[0]) // 2
+            return (self.minheap[0] - self.maxheap[0]) / 2
         return -self.maxheap[0]
 
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+
+medianFinder = MedianFinder()
+medianFinder.addNum(1)
+medianFinder.addNum(2)
+print(medianFinder.findMedian())
+medianFinder.addNum(3)
+print(medianFinder.findMedian())
