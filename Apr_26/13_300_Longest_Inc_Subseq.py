7d8a487f6d1cbd90c5b54882e6085e49c3fa3b69
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-13 21:27:27 +0530

    Using DP with no extra space

--- Apr_26/13_300_Longest_Inc_Subseq.py
+++ Apr_26/13_300_Longest_Inc_Subseq.py
@@ -1,21 +1,17 @@
 class Solution(object):
     def lengthOfLIS(self, nums):
         n = len(nums)
-        def solve(ind, curr):
+        def solve(ind, last, ans):
             if ind >= n:
-                return len(curr)
+                return ans
             take, skip = -1, -1
-            if not curr:
-                curr.append(nums[ind])
-                take = solve(ind + 1, curr)
-                curr.pop()
+            if last == -1:
+                take = solve(ind + 1, ind, ans + 1)
             else:
-                if nums[ind] > curr[-1]:
-                    curr.append(nums[ind])
-                    take = solve(ind + 1, curr)
-                    curr.pop()
-            skip = solve(ind + 1, curr)
+                if nums[ind] > nums[last]:
+                    take = solve(ind + 1, ind, ans + 1)
+            skip = solve(ind + 1, last, ans)
             return max(take, skip)
-        return solve(0, [])
+        return solve(0, -1, 0)
 
 print(Solution().lengthOfLIS(nums = [0,1,0,3,2,3]))
