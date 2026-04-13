6fab60b8a1d05554cd0e8126a5923ed06f995874
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-13 22:28:47 +0530

    Using DP with memoization bottom up fix

--- Apr_26/13_300_Longest_Inc_Subseq.py
+++ Apr_26/13_300_Longest_Inc_Subseq.py
@@ -1,19 +1,18 @@
-class Solution:
-    def lengthOfLIS(self, nums) -> int:
+class Solution(object):
+    def lengthOfLIS(self, nums):
         n = len(nums)
-        memo = [[-1] *( n + 1 ) for _ in range(n + 1)]
+        memo = [[-1] * n for _ in range(n + 1)]
 
-        def solve(ind, prev):
+        def solve(ind, last):
             if ind >= n:
                 return 0
-            if memo[ind][prev] == -1:
-                take, skip = 0, 0
-                if prev == -1 or nums[ind] > nums[prev]:
+            if memo[ind][last + 1] == -1:
+                take, skip = -1, -1
+                if last == -1 or nums[ind] > nums[last]:
                     take = 1 + solve(ind + 1, ind)
-                skip = solve(ind + 1, prev)
-                memo[ind][prev] = max(take, skip)
-            return memo[ind][prev]
-
+                skip = solve(ind + 1, last)
+                memo[ind][last + 1] = max(take, skip)
+            return memo[ind][last + 1]
         return solve(0, -1)
 
 print(Solution().lengthOfLIS(nums = [3,5,6,2,5,4,19,5,6,7,12]))
