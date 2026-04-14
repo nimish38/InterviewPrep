b5a298e0fd68b3325307b9bc117606d2981d21b9
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-14 20:46:47 +0530

    using dp with bottom up memoization

--- Apr_26/14_322_Coin_Change.py
+++ Apr_26/14_322_Coin_Change.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def coinChange(self, coins, amount):
+        memo = [-1] * (amount + 1)
         def solve(amt):
             if amt == 0:
                 return 0
-            res = float('inf')
-            for coin in coins:
-                if amt >= coin:
-                    res = min(res, 1 + solve(amt - coin))
-            return res
+            if memo[amt] == -1:
+                res = float('inf')
+                for coin in coins:
+                    if amt >= coin:
+                        res = min(res, 1 + solve(amt - coin))
+                memo[amt] = res
+            return memo[amt]
         ans = solve(amount)
         return -1 if ans == float('inf') else ans
 
+print(Solution().coinChange(coins = [1,2,5], amount = 11))
