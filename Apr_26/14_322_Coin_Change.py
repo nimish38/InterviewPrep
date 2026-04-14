af5ce3f9c84ad68e4dc0c4fda9bf25710d3f5f00
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-14 20:54:08 +0530

    using dp with top down

--- Apr_26/14_322_Coin_Change.py
+++ Apr_26/14_322_Coin_Change.py
@@ -1,17 +1,13 @@
 class Solution(object):
     def coinChange(self, coins, amount):
-        memo = [-1] * (amount + 1)
-        def solve(amt):
-            if amt == 0:
-                return 0
-            if memo[amt] == -1:
-                res = float('inf')
-                for coin in coins:
-                    if amt >= coin:
-                        res = min(res, 1 + solve(amt - coin))
-                memo[amt] = res
-            return memo[amt]
-        ans = solve(amount)
-        return -1 if ans == float('inf') else ans
+        memo = [float('inf')] * (amount + 1)
+        memo[0] = 0
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                diff = i - coin
+                if diff < 0:
+                    break
+                memo[i] = min(memo[i], 1 + memo[diff])
+        return memo[amount] if memo[amount] != float('inf') else -1
 
 print(Solution().coinChange(coins = [1,2,5], amount = 11))
