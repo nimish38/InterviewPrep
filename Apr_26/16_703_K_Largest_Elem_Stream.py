2629f8534e8ac39075f329f7341aca5c1790d014
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-16 21:09:12 +0530

    debug and fix via test case

--- Apr_26/16_703_K_Largest_Elem_Stream.py
+++ Apr_26/16_703_K_Largest_Elem_Stream.py
@@ -5,6 +5,9 @@ class KthLargest(object):
 
     def __init__(self, k, nums):
         self.nums, self.k = nums, k
+        heapq.heapify(self.nums)
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
 
     def add(self, val):
         if len(self.nums) + 1 < self.k:
@@ -12,3 +15,11 @@ class KthLargest(object):
         else:
             heapq.heappushpop(self.nums, val)
         return self.nums[0]
+
+
+kthLargest = KthLargest(3, [4, 5, 8, 2])
+print(kthLargest.add(3))
+print(kthLargest.add(5))
+print(kthLargest.add(10))
+print(kthLargest.add(9))
+print(kthLargest.add(4))
