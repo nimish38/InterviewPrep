7479d048031b3648a39a1005c01faaddbe27c981
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-17 21:27:14 +0530

    Using linear dp with memoization

--- Apr_26/17_416_Partition_Equal_Subset.py
+++ Apr_26/17_416_Partition_Equal_Subset.py
@@ -4,17 +4,21 @@ class Solution(object):
         if target % 2:
             return False
         nums.sort()
+        ans = target // 2
+        memo = [[-1] * (ans + 1) for _ in range(n)]
         def solve(ind, curr):
             if ind == n:
                 return False
-            take, skip, combo = False, False, curr + nums[ind]
-            if combo == ans:
-                return True
-            if combo < ans:
-                take = solve(ind + 1, combo)
-            skip = solve(ind + 1, curr)
-            return take or skip
-        ans = target // 2
+            if memo[ind][curr] == -1:
+                take, skip, combo = False, False, curr + nums[ind]
+                if combo == ans:
+                    memo[ind][curr] = True
+                else:
+                    if combo < ans:
+                        take = solve(ind + 1, combo)
+                    skip = solve(ind + 1, curr)
+                    memo[ind][curr] =  take or skip
+            return memo[ind][curr]
         return solve(0, 0)
 
 print(Solution().canPartition( nums = [2,5,12,5]))
