9cc4b39169fc1d9e58ed0d78a958f2f52668662e
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-18 21:41:23 +0530

    test with sample cases

--- Apr_26/18_876_Middle_Of_LL.py
+++ Apr_26/18_876_Middle_Of_LL.py
@@ -6,10 +6,13 @@ class ListNode(object):
 class Solution(object):
     def middleNode(self, head):
         slow, fast = head, head
-        while fast.next.next:
+        while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
         if fast.next:
             return slow.next
         return slow
 
+a, b, c, d, e = ListNode(1), ListNode(2), ListNode(3), ListNode(4), ListNode(5)
+a.next, b.next, c.next, d.next = b, c, d, e
+print(Solution().middleNode(a).val)
