e1044de557c81c54e11d4ae268f0939bf01b9502
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-19 13:44:44 +0530

    copied solution

--- Apr_26/19_421_Max_XOR_Of_2_Nums.py
+++ Apr_26/19_421_Max_XOR_Of_2_Nums.py
@@ -1,30 +1,30 @@
-class Solution(object):
-    def findMaximumXOR(self, nums):
-        trie, res = {}, 0
+import math
+class Solution:
+  def findMaximumXOR(self, nums) -> int:
+    maxNum = max(nums)
+    if maxNum == 0:
+      return 0
+    maxBit = int(math.log2(maxNum))
+    ans = 0
+    mask = 0
 
-        def insert(num):
-            curr, bit = trie, -1
-            for i in range(31, -1, -1):
-                bit = (num >> i) & 1
-                if bit not in curr:
-                    curr[bit] = {}
-                curr = curr[bit]
+    # If ans is 11100 when i = 2, it means that before we reach the last two
+    # bits, 11100 is the maximum XOR we have, and we're going to explore if we
+    # can get another two '1's and put them into ans.
+    for i in range(maxBit, -1, -1):
+      # Mask grows like: 100...000, 110...000, 111...000, ..., 111...111.
+      mask |= 1 << i
+      # We only care about the left parts,
+      # If i = 2, nums = [1110, 1011, 0111]
+      #    -> prefixes = [1100, 1000, 0100]
+      prefixes = set([num & mask for num in nums])
+      # If i = 1 and before this iteration, the ans is 10100, it means that we
+      # want to grow the ans to 10100 | 1 << 1 = 10110 and we're looking for
+      # XOR of two prefixes = candidate.
+      candidate = ans | 1 << i
+      for prefix in prefixes:
+        if prefix ^ candidate in prefixes:
+          ans = candidate
+          break
 
-        def getMax(num):
-            curr, bit, val = trie, -1, 0
-            for i in range(31, -1, -1):
-                bit = (num >> i) & 1
-                if 1 - bit in curr:
-                    curr = curr[1 - bit]
-                    val = (1 << i) | val
-                else:
-                    curr = curr[bit]
-            return val
-
-        for _ in nums:
-            insert(_)
-        for _ in nums:
-            res = max(res, getMax(_))
-        return res
-
-print(Solution().findMaximumXOR(nums = [3,10,5,25,2,8]))
+    return ans
