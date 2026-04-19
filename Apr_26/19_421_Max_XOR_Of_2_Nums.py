40d1f5dd9d93f448fad2bae3875f7e0bfaea1ff3
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-19 13:34:41 +0530

    using trie solution

--- Apr_26/19_421_Max_XOR_Of_2_Nums.py
+++ Apr_26/19_421_Max_XOR_Of_2_Nums.py
@@ -1,9 +1,30 @@
 class Solution(object):
     def findMaximumXOR(self, nums):
-        res = -1
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                res = max(res, nums[i] ^ nums[j])
+        trie, res = {}, 0
+
+        def insert(num):
+            curr, bit = trie, -1
+            for i in range(31, -1, -1):
+                bit = (num >> i) & 1
+                if bit not in curr:
+                    curr[bit] = {}
+                curr = curr[bit]
+
+        def getMax(num):
+            curr, bit, val = trie, -1, 0
+            for i in range(31, -1, -1):
+                bit = (num >> i) & 1
+                if 1 - bit in curr:
+                    curr = curr[1 - bit]
+                    val = (1 << i) | val
+                else:
+                    curr = curr[bit]
+            return val
+
+        for _ in nums:
+            insert(_)
+        for _ in nums:
+            res = max(res, getMax(_))
         return res
 
 print(Solution().findMaximumXOR(nums = [3,10,5,25,2,8]))
