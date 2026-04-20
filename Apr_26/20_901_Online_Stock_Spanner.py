39e6d29938561149bf280b2d893b9cfa266d724f
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-20 21:13:16 +0530

    test with sample cases

--- Apr_26/20_901_Online_Stock_Spanner.py
+++ Apr_26/20_901_Online_Stock_Spanner.py
@@ -4,6 +4,10 @@ class StockSpanner:
         self.st, self.cnt = [], 0
 
     def next(self, price: int):
+        if not self.st:
+            self.st.append((price, self.cnt))
+            self.cnt += 1
+            return 1
         while self.st and self.st[-1][0] < price:
             self.st.pop()
         last = 0
@@ -11,6 +15,14 @@ class StockSpanner:
             last = self.st[-1][1]
         self.st.append((price, self.cnt))
         self.cnt += 1
-        return self.cnt - last
+        return self.cnt - last - 1
 
 
+obj = StockSpanner()
+print(obj.next(100))
+print(obj.next(80))
+print(obj.next(60))
+print(obj.next(70))
+print(obj.next(60))
+print(obj.next(75))
+print(obj.next(85))
