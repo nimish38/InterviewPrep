6905de437a71612fa59d2f415e5b22f46dde6da6
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-21 21:25:49 +0530

    attempt using backtracking

---
+++ Apr_26/21_1373_Max_Sum_BST.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def maxSumBST(self, root):
+        self.best= 0
+        def solve(node):
+            if not node:
+                return 0, True
+            left, right = solve(node.left),solve(node.right)
+            isBST = left[1] and right[1]
+            if node.left and node.left.val > node.val:
+                isBST = False
+            if node.right and node.right.val < node.val:
+                isBST = False
+            if isBST:
+                value = left[0] + right[0] + node.val
+                self.best = max(self.best, value)
+                return value, True
+            return 0, False
+
+        solve(root)
+        return self.best
