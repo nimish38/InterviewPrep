32787a30edc44af63d27ba00771e0fa56019cfb7
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-21 21:45:06 +0530

    debug and fix with test cases

--- Apr_26/21_1373_Max_Sum_BST.py
+++ Apr_26/21_1373_Max_Sum_BST.py
@@ -13,15 +13,18 @@ class Solution(object):
                 return 0, True
             left, right = solve(node.left),solve(node.right)
             isBST = left[1] and right[1]
-            if node.left and node.left.val > node.val:
-                isBST = False
-            if node.right and node.right.val < node.val:
+            if not isBST:
+                return 0, False
+            if node.left and node.left.val >= node.val or node.right and node.right.val <= node.val:
                 isBST = False
             if isBST:
                 value = left[0] + right[0] + node.val
                 self.best = max(self.best, value)
                 return value, True
             return 0, False
-
         solve(root)
         return self.best
+
+a, b, c, d, e, f, g, h, i = TreeNode(1), TreeNode(4), TreeNode(3), TreeNode(2), TreeNode(4), TreeNode(2), TreeNode(5), TreeNode(4), TreeNode(6)
+a.left, a.right, b.left, b.right, c.left, c.right, g.left, g.right = b, c, d, e, f, g, h , i
+print(Solution().maxSumBST(a))
