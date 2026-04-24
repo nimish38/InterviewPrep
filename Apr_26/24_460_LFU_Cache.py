2fb02f336b9fe38c584ba4e742d327b908c388cb
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-24 23:56:42 +0530

    create skeleton logic

--- Apr_26/24_460_LFU_Cache.py
+++ Apr_26/24_460_LFU_Cache.py
@@ -1,26 +1,33 @@
-
 class DLL(object):
-    def __init__(self, key, val, count):
+    def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.count = count
         self.next = None
         self.prev = None
 
 class LFUCache(object):
     def __init__(self, capacity):
-        self.map, self.head, self.tail = {}, DLL(-1,-1,-1), DLL(-1,-1,-1)
-        self.head.next, self.tail.prev = self.tail, self.head
+        self.map, self.links, self.cap = {}, {}, capacity
 
     def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
+        if key not in self.map:
+            return -1
+        cnt, node = self.map[key]
+        self.removeNode(cnt, node)
+        self.addNode(cnt + 1, node)
+        self.map[key][1] += 1
+        return node.val
 
     def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
+        if key not in self.map:
+            if len(self.map) == self.cap:
+                self.removeLFU()
+            node = DLL(key, value)
+            self.map[key] = [1, node]
+            self.addNode(1, node)
+        else:
+            cnt, node = self.map[key]
+            self.removeNode(cnt, node)
+            self.addNode(cnt + 1, node)
+            node.val = value
+
