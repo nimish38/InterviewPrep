1b4b15a7bacb07d96b93e0c8c2e24b60e78855ae
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-25 00:13:13 +0530

    add helper functions

--- Apr_26/24_460_LFU_Cache.py
+++ Apr_26/24_460_LFU_Cache.py
@@ -31,3 +31,36 @@ class LFUCache(object):
             self.addNode(cnt + 1, node)
             node.val = value
 
+    def removeLFU(self):
+        head, tail =  self.links[0]
+        lfu_node = tail.prev
+        tail.prev, lfu_node.prev.next = lfu_node.prev, tail
+        head.val -= 1
+        if head.val == 0:
+            del self.links[0]
+        del self.map[lfu_node.key]
+
+    def addNode(self, cnt, node):
+        if cnt not in self.links:
+            head, tail = DLL('H', 1), DLL('T', -1)
+            head.next, tail.prev = node, node
+            node.next, node.prev = tail, head
+            self.links[cnt] = [head, tail]
+        else:
+            head, tail = self.links[cnt]
+            prev_mfu = head.next
+            head.next, prev_mfu.prev = node, node
+            node.next, node.prev = prev_mfu, head
+            head.val += 1
+
+    def removeNode(self, cnt, node):
+        head, tail = self.links[cnt]
+        if head.val == 1:
+            del self.links[cnt]
+        else:
+            l, r = node.prev, node.next
+            l.next, r.prev = r, l
+            del node
+            head.val -= 1
+
+
