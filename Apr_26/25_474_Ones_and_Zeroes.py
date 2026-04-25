57db230858e7c2f12ba964f8ddaf10a40e8c57cb
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-25 13:50:58 +0530

    using DP with memoization

--- Apr_26/25_474_Ones_and_Zeroes.py
+++ Apr_26/25_474_Ones_and_Zeroes.py
@@ -9,18 +9,25 @@ class Solution(object):
                 else:
                     o += 1
             return z, o
+        memo = []
+        for i in range(len(strs)):
+            x = []
+            for j in range(m + 1):
+                x.append([-1] * (n + 1))
+            memo.append(x)
 
         def solve(ind, zeros, ones):
             if ind >= len(strs):
                 return 0
-            take, skip= 0, 0
-            z, o = getVals(strs[ind])
-            if z <= zeros and o <= ones:
-                take = 1 + solve(ind + 1, zeros - z, ones - o)
-            skip = solve(ind + 1, zeros, ones)
-            return max(take, skip)
-
+            if memo[ind][zeros][ones] == -1:
+                take, skip= 0, 0
+                z, o = getVals(strs[ind])
+                if z <= zeros and o <= ones:
+                    take = 1 + solve(ind + 1, zeros - z, ones - o)
+                skip = solve(ind + 1, zeros, ones)
+                memo[ind][zeros][ones] = max(take, skip)
+            return memo[ind][zeros][ones]
         return solve(0, m, n)
 
 
-print(Solution().findMaxForm(strs = ["10001110","11000","111110"], m = 6, n = 6))
+print(Solution().findMaxForm(strs = ["10001110","11000","111110"], m = 5, n = 6))
