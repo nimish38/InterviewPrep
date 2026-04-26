75fd2ee741871c13711e0bfa2a8355c37e143557
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-26 15:26:42 +0530

    using builtin sorting and result in recursion

--- Apr_26/26_493_Reverse_Pairs.py
+++ Apr_26/26_493_Reverse_Pairs.py
@@ -1,39 +1,19 @@
-class Solution(object):
+class Solution:
     def reversePairs(self, nums):
-        self.res, n = 0, len(nums)
-
-        def divide(arr):
-            if len(arr) == 1:
-                return arr
+        def sort(arr):
+            if len(arr) <= 1:
+                return arr, 0
             mid = len(arr) // 2
-            left, right = divide(arr[:mid]), divide(arr[mid:])
-            return merge(left, right)
-
-        def merge(left, right):
-            l1, l2, i, j, arr = len(left), len(right), 0, 0, []
-            while i < l1 and j < l2:
-                if left[i] > 2 * right[j]:
+            left, c1 = sort(arr[:mid])
+            right, c2 = sort(arr[mid:])
+            count = c1 + c2
+            j = 0
+            for i in left:
+                while j < len(right) and i > 2 * right[j]:
                     j += 1
-                else:
-                    self.res += j
-                    i += 1
-            if i < l1:
-                self.res += (l1 - i) * j
-            i, j = 0, 0
-            while i < l1 and j < l2:
-                if left[i] <= right[j]:
-                    arr.append(left[i])
-                    i += 1
-                else:
-                    arr.append(right[j])
-                    j += 1
-            if i < l1:
-                arr.extend(left[i:])
-            if j < l2:
-                arr.extend(right[j:])
-            return arr
+                count += j
+            return sorted(left + right), count
 
-        divide(nums)
-        return self.res
+        return sort(nums)[1]
 
 print(Solution().reversePairs(nums = [1,3,2,3,1]))
