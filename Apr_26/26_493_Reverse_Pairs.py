77e680b3beb15d573a7593e0cb2fa12026e19c7e
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-26 15:23:59 +0530

    Calculate pairs before merging

--- Apr_26/26_493_Reverse_Pairs.py
+++ Apr_26/26_493_Reverse_Pairs.py
@@ -12,21 +12,28 @@ class Solution(object):
         def merge(left, right):
             l1, l2, i, j, arr = len(left), len(right), 0, 0, []
             while i < l1 and j < l2:
+                if left[i] > 2 * right[j]:
+                    j += 1
+                else:
+                    self.res += j
+                    i += 1
+            if i < l1:
+                self.res += (l1 - i) * j
+            i, j = 0, 0
+            while i < l1 and j < l2:
                 if left[i] <= right[j]:
                     arr.append(left[i])
                     i += 1
                 else:
-                    if left[i] > 2 * right[j]:
-                        self.res += (l1 - i)
                     arr.append(right[j])
                     j += 1
             if i < l1:
-                arr.extend(arr[i:])
+                arr.extend(left[i:])
             if j < l2:
-                arr.extend(arr[j:])
+                arr.extend(right[j:])
             return arr
 
         divide(nums)
         return self.res
 
-print(Solution().reversePairs(nums = [2,4,3,5,1]))
+print(Solution().reversePairs(nums = [1,3,2,3,1]))
