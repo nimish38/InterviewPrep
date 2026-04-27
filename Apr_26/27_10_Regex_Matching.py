e9463243a2262be95745e78fcb85986689f0a7e8
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-28 00:39:38 +0530

    using dp template attempt and fix

--- Apr_26/27_10_Regex_Matching.py
+++ Apr_26/27_10_Regex_Matching.py
@@ -1,23 +1,22 @@
 class Solution(object):
     def isMatch(self, s, p):
         def solve(i, j):
-            if i == len(s) and j == len(p):
-                return True
-            if i == len(s) or j == len(p):
+            if j >= len(p):
+                if i >= len(s):
+                    return True
                 return False
-            if p[j] == '.' or (p[j] == s[i]):
-                return solve(i + 1, j + 1)
-            elif p[j] == '*':
-                c, res, k = s[i], solve(i, j + 1), 1
-                while i + k < len(s) and s[i + k] == c:
-                    if solve(i + k, j + 1):
-                        return True
-                    k += 1
-                return solve(i + k, j + 1)
+            if j + 1 < len(p) and p[j + 1] == '*':
+                take = False
+                if i < len(s) and (p[j] == '.' or p[j] == s[i]):
+                    take = solve(i + 1, j)
+                skip = solve(i, j + 2)
+                return take or skip
             else:
-                return solve(i, j + 1)
+                if i < len(s) and (p[j] == '.' or p[j] == s[i]):
+                    return solve(i + 1, j + 1)
+                return False
 
         return solve(0, 0)
 
-print(Solution().isMatch(s = "aab", p = "c*a*b"))
+print(Solution().isMatch(s = "ab", p = ".*"))
 
