b49569073856e341908b08e051d0ca41b36d5623
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-28 00:44:41 +0530

    using dp template attempt and fix and memoization

--- Apr_26/27_10_Regex_Matching.py
+++ Apr_26/27_10_Regex_Matching.py
@@ -1,20 +1,24 @@
 class Solution(object):
     def isMatch(self, s, p):
+        memo = [[-1] * (len(s) + 1) for _ in range(len(p) + 1)]
         def solve(i, j):
             if j >= len(p):
                 if i >= len(s):
                     return True
                 return False
-            if j + 1 < len(p) and p[j + 1] == '*':
-                take = False
-                if i < len(s) and (p[j] == '.' or p[j] == s[i]):
-                    take = solve(i + 1, j)
-                skip = solve(i, j + 2)
-                return take or skip
-            else:
-                if i < len(s) and (p[j] == '.' or p[j] == s[i]):
-                    return solve(i + 1, j + 1)
-                return False
+            if memo[i][j] == -1:
+                if j + 1 < len(p) and p[j + 1] == '*':
+                    take = False
+                    if i < len(s) and (p[j] == '.' or p[j] == s[i]):
+                        take = solve(i + 1, j)
+                    skip = solve(i, j + 2)
+                    memo[i][j] = take or skip
+                else:
+                    if i < len(s) and (p[j] == '.' or p[j] == s[i]):
+                        memo[i][j] = solve(i + 1, j + 1)
+                    else:
+                        memo[i][j] = False
+            return memo[i][j]
 
         return solve(0, 0)
 
