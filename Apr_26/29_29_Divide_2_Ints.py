f1735d6e6c00e8ea80194aa790d119ac6279f253
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-29 23:01:17 +0530

    using binary notation logic

--- Apr_26/29_29_Divide_2_Ints.py
+++ Apr_26/29_29_Divide_2_Ints.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def divide(self, dividend, divisor):
-        sign, upper, lower = 1, 2147483647, -2147483648
-        if dividend < 0:
-            dividend = -dividend
-            sign = -sign
-        if divisor < 0:
-            divisor = -divisor
-            sign = -sign
-        res = sign * (dividend // divisor)
+        sign, res, upper, lower = 1, 0, 2147483647, -2147483648
+        if (dividend < 0 < divisor) or (dividend > 0 > divisor):
+            sign = -1
+        divisor, dividend = abs(divisor), abs(dividend)
+        while dividend >= divisor:
+            i = 0
+            while divisor * (2 ** i) <= dividend:
+                i += 1
+            i -= 1
+            res += 2 ** i
+            dividend -= divisor * (2 ** i)
+        res = sign * res
         if res > upper:
             return upper
         if res < lower:
