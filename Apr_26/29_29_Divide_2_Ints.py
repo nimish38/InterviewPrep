2ab28d7ca081f5f8c401fd6211329e989acc95b8
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-29 23:08:19 +0530

    replace multiplication and pow by bit operations

--- Apr_26/29_29_Divide_2_Ints.py
+++ Apr_26/29_29_Divide_2_Ints.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def divide(self, dividend, divisor):
-        sign, res, upper, lower = 1, 0, 2147483647, -2147483648
+        sign, res, upper, lower = True, 0, 2147483647, -2147483648
         if (dividend < 0 < divisor) or (dividend > 0 > divisor):
-            sign = -1
+            sign = False
         divisor, dividend = abs(divisor), abs(dividend)
         while dividend >= divisor:
             i = 0
-            while divisor * (2 ** i) <= dividend:
+            while divisor << (i + 1) <= dividend:
                 i += 1
-            i -= 1
-            res += 2 ** i
-            dividend -= divisor * (2 ** i)
-        res = sign * res
+            res += 1 << i
+            dividend -= divisor << i
+        if not sign:
+            res = -res
         if res > upper:
             return upper
         if res < lower:
