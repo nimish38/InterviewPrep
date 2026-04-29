8c9943f350f18c0f721117b0ceef9a9b8fa093db
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-29 22:24:21 +0530

    brute force with sign and range check

--- Apr_26/29_29_Divide_2_Ints.py
+++ Apr_26/29_29_Divide_2_Ints.py
@@ -1,10 +1,15 @@
 class Solution(object):
     def divide(self, dividend, divisor):
-        sign = 1
+        sign, upper, lower = 1, 2147483647, -2147483648
         if dividend < 0:
             dividend = -dividend
             sign = -sign
         if divisor < 0:
             divisor = -divisor
             sign = -sign
-        return sign * (dividend // divisor)
+        res = sign * (dividend // divisor)
+        if res > upper:
+            return upper
+        if res < lower:
+            return lower
+        return res
