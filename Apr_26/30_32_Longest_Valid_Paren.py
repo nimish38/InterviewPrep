a0e4c7e136df99ec29c7c93030ec0c5fef04c1c7
nimish38 <nimish.v.shinde60@gmail.com>
2026-04-30 13:24:40 +0530

    using stack and also pushing closing

--- Apr_26/30_32_Longest_Valid_Paren.py
+++ Apr_26/30_32_Longest_Valid_Paren.py
@@ -3,11 +3,14 @@ class Solution(object):
         st, best = [], 0
         for i in range(len(s)):
             if s[i] =='(':
-                st.append(i)
-            elif s[i] == ')' and st:
-                st.pop()
-                last = 0 if not st else st[-1]
-                best = max(best, i - last)
+                st.append(('(', i))
+            else:
+                if st and st[-1][0] == '(':
+                    st.pop()
+                    last = -1 if not st else st[-1][1]
+                    best = max(best, i - last)
+                else:
+                    st.append((')', i))
         return best
 
 print(Solution().longestValidParentheses(s = ")()())"))
