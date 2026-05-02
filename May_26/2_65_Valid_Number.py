3d381f429abb9340e7e38b5101bb369ce563ef90
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-02 16:41:08 +0530

    validations

--- May_26/2_65_Valid_Number.py
+++ May_26/2_65_Valid_Number.py
@@ -1,29 +1,35 @@
 class Solution(object):
     def isNumber(self, s):
-        sign1, sign2, expo, deci, nums, left, right = False, False, False, False, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, False, False
-        for c in s:
+        sign1, sign2, expo, deci, nums, left, right = False, False, False, False, {'0', '1','2', '3', '4', '5', '6', '7', '8', '9'}, False, False
+        for i in range(len(s)):
+            c = s[i]
             if c == '+' or c =='-':
+                if i != 0 or s[i - 1] != 'e' or s[i - 1] != 'E':
+                    return False
                 if not sign1:
                     sign1 = True
                 elif expo and not sign2:
                     sign2 = True
                 else:
                     return False
-            if c == '.':
+            elif c == '.':
                 if not deci and not expo:
                     deci = True
                 else:
                     return False
-            if c == 'e' or c == 'E':
-                if not expo:
+            elif c == 'e' or c == 'E':
+                if left and not expo:
                     expo = True
                 else:
                     return False
-            if c in nums:
+            elif c in nums:
                 if not expo and not left:
                     left = True
                 if expo and not right:
                     right = True
             else:
                 return False
-        return left and right
+        return left if not expo else left and right
+
+
+print(Solution().isNumber("95"))
