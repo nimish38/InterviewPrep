6a45572fc541e57daec505ebd3843c4867124939
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-03 15:15:26 +0530

    Convert 2d into 1d matrix and get max area

---
+++ May_26/3_85_Maximal_Rectangle.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        row = matrix[0].copy()
+
+        def getNSE(row):
+            st, arr = [], [n] * n
+            for i in range(n - 1, -1, -1):
+                if st:
+                    arr[i] = st[-1]
+                while st and row[i] <= row[st[-1]]:
+                    st.pop()
+                st.append(i)
+            return arr
+
+        def getMaxArea(row):
+            nse, pse, curr = getNSE(row), getPSE(row), 0
+            for i in range(n):
+                curr = max(curr, row[i] * (nse[i] - pse[i] - 1))
+            return curr
+
+        area = getMaxArea(row)
+        for r in range(1, m):
+            for c in range(n):
+                if matrix[r][c]:
+                    row[c] += 1
+                else:
+                    row[c] = 0
+            area = max(area, getMaxArea(row))
+        return area
