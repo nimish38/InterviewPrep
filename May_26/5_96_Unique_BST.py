f09df05ef0400281e53ac0df29b50b1610677c2d
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-05 21:51:33 +0530

    Using DP top down

--- May_26/5_96_Unique_BST.py
+++ May_26/5_96_Unique_BST.py
@@ -1,16 +1,11 @@
 class Solution(object):
     def numTrees(self, n):
-        memo = [[-1] * n for _ in range(n)]
-        def genBST(l, r):
-            if l > r:
-                return 1
-            if memo[l - 1][r - 1] == -1:
-                res = 0
-                for i in range(l, r + 1):
-                    X,Y = genBST(l, i - 1), genBST(i + 1, r)
-                    res += X * Y
-                memo[l - 1][r - 1] = res
-            return memo[l - 1][r - 1]
-        return genBST(1, n)
+        memo = [0] * (n + 1)
+        memo[0] = memo[1] = 1
+        for nodes in range(2, n + 1):
+            for root in range(1, nodes + 1):
+                left, right = root - 1, nodes - root
+                memo[nodes] += memo[left] * memo[right]
+        return memo[n]
 
 print(Solution().numTrees(5))
