f14b1459f743731fcad1e45b4ae01fe5ef6450d7
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-05 21:38:58 +0530

    Using DP bottom up with memoization

--- May_26/5_96_Unique_BST.py
+++ May_26/5_96_Unique_BST.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def numTrees(self, n):
+        memo = [[-1] * n for _ in range(n)]
         def genBST(l, r):
             if l > r:
                 return 1
-            res = 0
-            for i in range(l, r + 1):
-                X,Y = genBST(l, i - 1), genBST(i + 1, r)
-                for dx in range(X):
-                    for dy in range(Y):
-                        res += 1
-            return res
+            if memo[l - 1][r - 1] == -1:
+                res = 0
+                for i in range(l, r + 1):
+                    X,Y = genBST(l, i - 1), genBST(i + 1, r)
+                    res += X * Y
+                memo[l - 1][r - 1] = res
+            return memo[l - 1][r - 1]
         return genBST(1, n)
 
-print(Solution().numTrees(4))
+print(Solution().numTrees(5))
