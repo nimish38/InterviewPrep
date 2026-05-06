b7aff12204578930ce3c04e198b2019c8ae486e7
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-06 22:45:26 +0530

    get inorder, sort and then swap values

---
+++ May_26/6_99_Recover_BST.py
@@ -0,0 +1,27 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def recoverTree(self, root):
+        nodes, curr, inorder, st = {}, root, [], []
+        while curr:
+            st.append(curr)
+            curr = curr.left
+        while st:
+            curr = st.pop()
+            inorder.append(curr.val)
+            nodes[curr.val] = curr
+            if curr.right:
+                curr = curr.right
+                while curr:
+                    st.append(curr)
+                    curr = curr.left
+        correct_inorder = sorted(inorder.copy())
+        for _ in range(len(inorder)):
+            if inorder[_] != correct_inorder[_]:
+                nodes[inorder[_]].val, nodes[correct_inorder[_]].val = correct_inorder[_], inorder[_]
+                break
+        return root
