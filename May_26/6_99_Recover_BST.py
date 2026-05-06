b20c38d1027325b00c40062d8c67c12aae21435e
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-06 22:48:28 +0530

    test with sample case

--- May_26/6_99_Recover_BST.py
+++ May_26/6_99_Recover_BST.py
@@ -25,3 +25,9 @@ class Solution(object):
                 nodes[inorder[_]].val, nodes[correct_inorder[_]].val = correct_inorder[_], inorder[_]
                 break
         return root
+
+
+a, b, c, d = TreeNode(3), TreeNode(1), TreeNode(4), TreeNode(2)
+a.left, a.right, c.left = b, c, d
+z = Solution().recoverTree(a)
+print(z)
