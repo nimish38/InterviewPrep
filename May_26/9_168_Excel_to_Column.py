a1fc5ef6a684041a2fd960433ab6bbb545ac1890
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-09 22:20:31 +0530

    Fix as it base 26 after subtracting 1

--- May_26/9_168_Excel_to_Column.py
+++ May_26/9_168_Excel_to_Column.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def convertToTitle(self, columnNumber):
         res = ''
-        while columnNumber > 26:
+        while columnNumber > 0:
+            columnNumber -= 1
             rem, columnNumber = columnNumber % 26, columnNumber // 26
-            res = chr(64 + rem) + res
-        return chr(64 + columnNumber) + res
+            res = chr(65 + rem) + res
+        return res
 
-print(Solution().convertToTitle(703))
+print(Solution().convertToTitle(701))
