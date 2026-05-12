290c0b887b9947e94a75d0ec1db679906dbde172
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-12 22:38:47 +0530

    optimal, remove spaces and keep track of last function

--- May_26/12_227_Basic_Calculator_II.py
+++ May_26/12_227_Basic_Calculator_II.py
@@ -1,39 +1,33 @@
-import operator
-
-
 class Solution(object):
     def calculate(self, s):
-        st, i, operations, num = [], 0, {'+': operator.add, '-':operator.sub, '*':operator.mul, '/':operator.floordiv}, ''
-        while i < len(s):
-            if s[i] in operations:
-                if num:
-                    st.append(int(num))
-                    num = ''
-                if s[i] == '*' or s[i] == '/':
-                    oper, op1, i = operations[s[i]], st.pop(), i + 1
-                    while s[i] == ' ':
-                        i += 1
-                    while i < len(s) and s[i] not in operations and s[i] != ' ':
-                        num += s[i]
-                        i += 1
-                    st.append(oper(op1, int(num)))
-                    num, i = '', i - 1
-                else:
-                    st.append(s[i])
-            elif s[i] == ' ':
-                if num:
-                    st.append(int(num))
-                    num = ''
+        """
+        :type s: str
+        :rtype: int
+        """
+        e = []
+        n = 0
+        f = '+'
+
+        s = s.replace(" ", "") + '+'
+
+        for c in s:
+            if c.isdigit():
+                n = n * 10 + int(c)
             else:
-                num += s[i]
-            i += 1
-        if num:
-            st.append(int(num))
+                if f == '+':
+                    e.append(n)
+                elif f == '-':
+                    e.append(-n)
+                elif f == '*':
+                    e.append(e.pop() * n)
+                elif f == '/':
+                    t = e.pop()
+                    if t < 0:
+                        e.append(- (abs(t) // n))  
+                    else:
+                        e.append(t // n)
 
-        res = st[0]
-        for i in range(1, len(st), 2):
-            oper, op2 = operations[st[i]], st[i + 1]
-            res = oper(res, op2)
-        return res
+                f = c
+                n = 0
 
-print(Solution().calculate(s = "3"))
+        return sum(e)
