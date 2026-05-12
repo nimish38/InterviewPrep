80b627a74e2a8af8c6569d92ebf24e35447f7583
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-12 22:27:01 +0530

    handle multiply and div earlier and push rest to stack

--- May_26/12_227_Basic_Calculator_II.py
+++ May_26/12_227_Basic_Calculator_II.py
@@ -3,15 +3,35 @@ import operator
 
 class Solution(object):
     def calculate(self, s):
-        st, i, operations = [], 0, {'+': operator.add, '-':operator.sub, '*':operator.mul, '/':operator.floordiv}
+        st, i, operations, num = [], 0, {'+': operator.add, '-':operator.sub, '*':operator.mul, '/':operator.floordiv}, ''
         while i < len(s):
-            if s[i] == '+' or s[i] == '-':
-                st.append(s[i])
-            elif s[i] == '*' or s[i] == '/':
-                op1, op2 = st.pop(), int(s[i + 1])
-                st.append(operations[s[i]](op1, op2))
-                i += 1
+            if s[i] in operations:
+                if num:
+                    st.append(int(num))
+                    num = ''
+                if s[i] == '*' or s[i] == '/':
+                    oper, op1, i = operations[s[i]], st.pop(), i + 1
+                    while s[i] == ' ':
+                        i += 1
+                    while i < len(s) and s[i] not in operations and s[i] != ' ':
+                        num += s[i]
+                        i += 1
+                    st.append(oper(op1, int(num)))
+                    num, i = '', i - 1
+                else:
+                    st.append(s[i])
             elif s[i] == ' ':
-                continue
+                if num:
+                    st.append(int(num))
+                    num = ''
             else:
-                st.append(int(s[i]))
+                num += s[i]
+            i += 1
+
+        res = st[0]
+        for i in range(1, len(st), 2):
+            oper, op2 = operations[st[i]], st[i + 1]
+            res = oper(res, op2)
+        return res
+
+print(Solution().calculate(s = "3+2*2"))
