117daee048ced9f67a8301dc79f1d80858c53dc0
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-14 21:37:42 +0530

    Using digit DP

--- May_26/14_233_No_Of_Digit_One.py
+++ May_26/14_233_No_Of_Digit_One.py
@@ -1,8 +1,19 @@
 class Solution(object):
     def countDigitOne(self, n):
-        cnt = 0
-        for _ in range(1, n + 1):
-            cnt += str(_).count('1')
-        return cnt
+        n = str(n)
+        def solve(ind, tight, cnt):
+            if ind == len(n):
+                return cnt
+            lower, res = 0, 0
+            if tight:
+                upper = int(n[ind])
+            else:
+                upper = 9
+            for dig in range(lower, upper + 1):
+                isTight, isOne = dig == upper, 1 if dig == 1 else 0
+                res += solve(ind + 1, tight and isTight, cnt + isOne)
+            return res
+
+        return solve(0, True, 0)
 
 print(Solution().countDigitOne(13))
