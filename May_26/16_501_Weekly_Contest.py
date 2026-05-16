2f2919bcfaab03cf001e1ea839ebb11beb6c5e5f
nimish38 <nimish.v.shinde60@gmail.com>
2026-05-16 16:03:32 +0530

    Q3

--- May_26/16_501_Weekly_Contest.py
+++ May_26/16_501_Weekly_Contest.py
@@ -1,13 +1,21 @@
+from math import isqrt
+
 class Solution(object):
     def minArraySum(self, nums):
-        nums.sort(reverse=True)
-        for i in range(len(nums) - 1):
-            j = len(nums) - 1
-            while i < j and nums[i] > nums[j]:
-                if nums[i] % nums[j] == 0:
-                    nums[i] = nums[j]
-                    break
-                j -= 1
-        return sum(nums)
+        present = set(nums)
+        total = 0
+        for x in nums:
+            best = x
+            for d in range(1, isqrt(x) + 1):
+                if x % d == 0:
+                    # First divisor
+                    if d in present:
+                        best = min(best, d)
+                    # Paired divisor
+                    other = x // d
+                    if other in present:
+                        best = min(best, other)
+            total += best
+        return total
 
-print(Solution().minArraySum(nums = [4,2,8,3]))
+print(Solution().minArraySum(nums = [14, 7]))
